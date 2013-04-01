ae0d92aba7f824e38cf9a0c208143984937fcc75
Sam Reid <reids@colorado.edu>
2013-03-31 23:29:30 -0600

    Removed unused code

--- js/CheckBox.js
+++ js/CheckBox.js
@@ -13,9 +13,6 @@ define( function( require ) {
     options.cursor = 'pointer';
     Node.call( this, options );
 
-    var width = 100;
-    var height = 100;
-
     var buttonShape = new Path( {shape: Shape.rect( 0, 0, content.width + 10, content.height + 10 ), stroke: 'black', lineWidth: 1, fill: 'orange'} );
     this.addChild( buttonShape );
     content.centerX = buttonShape.width / 2;
