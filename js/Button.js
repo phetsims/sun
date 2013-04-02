1b96c17a1091f8860e97e516dbc6cac5ead7c5d9
Sam Reid <reids@colorado.edu>
2013-04-02 13:35:37 -0600

    Switched from Path/Shape to SCENERY/Rectangle

--- js/Button.js
+++ js/Button.js
@@ -4,16 +4,15 @@ define( function( require ) {
   "use strict";
 
   var Node = require( 'SCENERY/nodes/Node' );
-  var Path = require( 'SCENERY/nodes/Path' );
-  var Shape = require( 'KITE/Shape' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
 
   function Button( content, options, callback ) {
     options = options || {};
     options.cursor = 'pointer';
     Node.call( this, options );
 
-    var buttonShape = new Path( {shape: Shape.roundRect( 0, 0, content.width + 10, content.height + 10, 10, 10 ), stroke: 'black', lineWidth: 1, fill: 'orange'} );
+    var buttonShape = new Rectangle( 0, 0, content.width + 10, content.height + 10, 10, 10, {stroke: 'black', lineWidth: 1, fill: 'orange'} );
     this.addChild( buttonShape );
     content.centerX = buttonShape.width / 2;
     content.centerY = buttonShape.height / 2;
