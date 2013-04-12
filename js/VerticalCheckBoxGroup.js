272621b4322ea0d016a3b70d177403ff398a4b91
Sam Reid <reids@colorado.edu>
2013-04-12 08:37:07 -0600

    Added vertical check box group

---
+++ js/VerticalCheckBoxGroup.js
@@ -0,0 +1,35 @@
+//Render a simple vertical check box group, where the buttons all have the same sizes
+//TODO: not ready for use in simulations, it will need further development & discussion first.
+define( function( require ) {
+  "use strict";
+
+  var Path = require( 'SCENERY/nodes/Path' );
+  var CheckBox = require( 'SUN/CheckBox' );
+  var VBox = require( 'SCENERY/nodes/VBox' );
+  var Shape = require( 'KITE/Shape' );
+  var inherit = require( 'PHET_CORE/inherit' );
+
+  function VerticalCheckBoxGroup( items, options ) {
+    options = options || {};
+
+    var width = 0;
+    for ( var i = 0; i < items.length; i++ ) {
+      width = Math.max( width, items[i].content.width );
+    }
+
+    var children = [];
+    for ( i = 0; i < items.length; i++ ) {
+
+      //Add an invisible strut to each content to make the widths match
+      var content = new Path( {shape: Shape.rect( 0, 0, width, 0 ), children: [items[i].content]} );
+      children.push( new CheckBox( content, items[i].property ) );
+    }
+
+    options.children = children;
+    VBox.call( this, options );
+  }
+
+  inherit( VerticalCheckBoxGroup, VBox );
+
+  return VerticalCheckBoxGroup;
+} );
