6f0c0fd617e46757e3ae83ad5b9ca8029af4421c
Chris Malley <cmalley@pixelzoom.com>
2013-04-29 13:36:55 -0600

    doc

--- js/RadioButton.js
+++ js/RadioButton.js
@@ -15,7 +15,7 @@ define( function ( require ) {
   /**
    * @param property
    * @param value the value that corresponds to this button, same type as property
-   * @param {Node} node node the will be vertically centered to the right of the button
+   * @param {Node} node node that will be vertically centered to the right of the button
    * @param {object} options
    * @constructor
    */
