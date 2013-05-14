1c6058b90bb25497adbe62a9204bb21024163ef9
Sam Reid <reids@colorado.edu>
2013-05-14 09:01:27 -0600

    minor doc fix

--- js/RadioButton.js
+++ js/RadioButton.js
@@ -16,7 +16,7 @@ define( function ( require ) {
   /**
    * @param property
    * @param value the value that corresponds to this button, same type as property
-   * @param {Node} node node that will be vertically centered to the right of the button
+   * @param {Node} node that will be vertically centered to the right of the button
    * @param {object} options
    * @constructor
    */
