c88c34a82512f6757090456ddb562f3ba440b401
Sam Reid <reids@colorado.edu>
2013-06-02 20:10:59 -0600

    ported Sun from Fort to PropertySet

--- js/ButtonModel.js
+++ js/ButtonModel.js
@@ -1,37 +1,23 @@
-/**Model for buttons
-
- Reasons to use Fort for ButtonModel
- 1. It is very clean and concise (create all attributes, listeners, notifications, preventing notification when value didn't actually change, etc with just a few lines of code)
- 2. It will match well with our other model code
- 3. It will automatically gain bonus features of Fort, such as record/playback
-
- Reasons not to use Fort for ButtonModel
- 1. Creates a dependency of SUN->Fort
- 2. Possible memory/performance problems?
-
- @author Sam Reid
+/**
+ * Model for button state
+ * @author Sam Reid
  */
 define( function( require ) {
   "use strict";
 
-  var Fort = require( 'FORT/Fort' );
+  var PropertySet = require( 'PHETCOMMON/model/property/PropertySet' );
+  var inherit = require( 'PHET_CORE/inherit' );
 
-  return Fort.Model.extend( {
-                              //The default state for the button
-                              defaults: {
-                                enabled: true,
-                                armed: false,
-                                pressed: false
-                              },
+  function ButtonModel() {
+    //The default state for the button
+    PropertySet.call( this, { enabled: true, armed: false, pressed: false} );
 
-                              //When the ButtonModel is created, create an untracked list of listeners.
-                              initialize: function() {
-                                Fort.Model.prototype.initialize.call( this );
-                                this.listeners = [];
-                              },
+    this.listeners = [];
+  }
 
-                              //Fire all of the listeners.
-                              fireListeners: function() {
-                                this.listeners.forEach( function( listener ) { listener(); } );
-                              }} );
+  return inherit( ButtonModel, PropertySet, {
+    //Fire all of the listeners.  TODO: Should this use backbone or jquery events, or is it already good?
+    fireListeners: function() {
+      this.listeners.forEach( function( listener ) { listener(); } );
+    }} );
 } );
