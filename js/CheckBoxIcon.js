7daaa2f180f38f881f5b541602416c07f4d116e6
Sam Reid <reids@colorado.edu>
2013-06-02 20:11:31 -0600

    updated docs

--- js/CheckBoxIcon.js
+++ js/CheckBoxIcon.js
@@ -1,5 +1,5 @@
 //Icon for a check box that shows as checked if the item is selected or unchecked if not
-//Uses a Fort.Model.property style interface, with 'link' method
+//Uses a phetcommon/Property style interface, with 'link' method
 //Uses Font Awesome for rendering the check boxes.  Please make sure font is loaded before calling this so the sizes will be correct on construction.
 define( function( require ) {
   "use strict";
