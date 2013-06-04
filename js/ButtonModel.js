9037f2a162d2ce18747d4245c38a78c3f98ab81e
Sam Reid <reids@colorado.edu>
2013-06-04 12:11:10 -0600

    converted from PropertySet to PropertySetB

--- js/ButtonModel.js
+++ js/ButtonModel.js
@@ -5,17 +5,17 @@
 define( function( require ) {
   "use strict";
 
-  var PropertySet = require( 'PHETCOMMON/model/property/PropertySet' );
+  var PropertySetB = require( 'PHETCOMMON/model/property/PropertySetB' );
   var inherit = require( 'PHET_CORE/inherit' );
 
   function ButtonModel() {
     //The default state for the button
-    PropertySet.call( this, { enabled: true, armed: false, pressed: false} );
+    PropertySetB.call( this, { enabled: true, armed: false, pressed: false} );
 
     this.listeners = [];
   }
 
-  return inherit( ButtonModel, PropertySet, {
+  return inherit( ButtonModel, PropertySetB, {
     //Fire all of the listeners.  TODO: Should this use backbone or jquery events, or is it already good?
     fireListeners: function() {
       this.listeners.forEach( function( listener ) { listener(); } );
