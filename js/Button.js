738ab5cecf83608258b3206144e9de9bbdecc425
Chris Malley <cmalley@pixelzoom.com>
2013-06-04 20:02:22 -0600

    integrate scenery.ButtonListener into sun.Button

--- js/Button.js
+++ js/Button.js
@@ -4,10 +4,10 @@ define( function( require ) {
   "use strict";
 
   var Node = require( 'SCENERY/nodes/Node' );
-  var DOM = require( 'SCENERY/nodes/DOM' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var ButtonModel = require( 'SUN/ButtonModel' );
+  var ButtonListener = require( 'SCENERY/input/ButtonListener' );
 
   /**
    * @param {Node} content
@@ -30,10 +30,10 @@ define( function( require ) {
                         options );
 
     var button = this;
-    Node.call( this, options );
+    Node.call( button, options );
 
-    this.buttonModel = new ButtonModel();
-    this.buttonModel.listeners.push( callback );
+    button.buttonModel = new ButtonModel();
+    button.buttonModel.listeners.push( callback );
 
     var path = new Rectangle( 0, 0, content.width + ( 2 * options.xMargin ), content.height + ( 2 * options.yMargin ), options.cornerRadius, options.cornerRadius,
                               {stroke: options.stroke, lineWidth: options.lineWidth, fill: options.fill } );
@@ -41,9 +41,13 @@ define( function( require ) {
     content.centerX = path.width / 2;
     content.centerY = path.height / 2;
     button.addChild( content );
-    button.addInputListener( {up: this.buttonModel.fireListeners.bind( this.buttonModel )} );
+    button.addInputListener( new ButtonListener( {
+      fire: function() {
+        button.buttonModel.fireListeners();
+      }
+    }) );
 
-    this.addPeer( '<input type="button">', {click: this.buttonModel.fireListeners.bind( this.buttonModel )} );
+    button.addPeer( '<input type="button">', {click: button.buttonModel.fireListeners.bind( button.buttonModel )} );
   }
 
   inherit( Button, Node, {
