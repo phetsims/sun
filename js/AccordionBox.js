b9004687a9addec6ca9a40c66fc017becb543db8
jbphet <john.blanco@colorado.edu>
2013-06-07 14:23:39 -0600

    Added gradients for the open/close buttons and improved the symbols used on those buttons.

--- js/AccordionBox.js
+++ js/AccordionBox.js
@@ -18,6 +18,7 @@ define( function( require ) {
   var Vector2 = require( 'DOT/Vector2' );
   var Property = require( 'AXON/Property' );
   var Shape = require( 'KITE/Shape' );
+  var LinearGradient = require( 'SCENERY/util/LinearGradient' );
 
   // Constants
   var CONTROL_BUTTON_INSET = 4; // Can make this an option if desired.
@@ -47,39 +48,54 @@ define( function( require ) {
     var open = new Property( options.initiallyOpen || true );
 
     // Create the open/close nodes.
-    var openNode = new Rectangle( CONTROL_BUTTON_INSET,
-                                  CONTROL_BUTTON_INSET,
-                                  CONTROL_BUTTON_DIMENSION,
-                                  CONTROL_BUTTON_DIMENSION,
-                                  3,
-                                  3,
-                                  {stroke: 'black', lineWidth: 1, fill: 'green', cursor: 'pointer' } );
-    var plusShape = new Shape().moveTo( -CONTROL_BUTTON_SYMBOL_WIDTH / 2, 0 ).
-      lineTo( CONTROL_BUTTON_SYMBOL_WIDTH / 2, 0 ).
-      moveTo( 0, -CONTROL_BUTTON_SYMBOL_WIDTH / 2 ).
-      lineTo( 0, CONTROL_BUTTON_SYMBOL_WIDTH / 2 );
+    var openNode = new Rectangle( 0, 0, CONTROL_BUTTON_DIMENSION, CONTROL_BUTTON_DIMENSION, 3, 3,
+                                  {
+                                    cursor: 'pointer',
+                                    top: CONTROL_BUTTON_INSET,
+                                    left: CONTROL_BUTTON_INSET,
+                                    fill: new LinearGradient( 0, 0, CONTROL_BUTTON_DIMENSION, CONTROL_BUTTON_DIMENSION ).
+                                      addColorStop( 0, 'rgb(0, 230, 0 )' ).
+                                      addColorStop( 1, 'rgb(0, 179, 0 )' )
+
+                                  } );
+//    var plusShape = new Shape().
+//      moveTo( -CONTROL_BUTTON_SYMBOL_WIDTH / 2, 0 ).
+//      lineTo( CONTROL_BUTTON_SYMBOL_WIDTH / 2, 0 ).
+//      moveTo( 0, -CONTROL_BUTTON_SYMBOL_WIDTH / 2 ).
+//      lineTo( 0, CONTROL_BUTTON_SYMBOL_WIDTH / 2 );
+    var plusShape = new Shape().
+      moveTo( CONTROL_BUTTON_SYMBOL_WIDTH / 2, 0 ).
+      lineTo( CONTROL_BUTTON_SYMBOL_WIDTH / 2, CONTROL_BUTTON_SYMBOL_WIDTH ).
+      moveTo( 0, CONTROL_BUTTON_SYMBOL_WIDTH / 2 ).
+      lineTo( CONTROL_BUTTON_SYMBOL_WIDTH, CONTROL_BUTTON_SYMBOL_WIDTH / 2 );
     openNode.addChild( new Path( { shape: plusShape,
                                    lineWidth: SYMBOL_LINE_WIDTH,
                                    stroke: 'white',
-                                   center: openNode.center } ) );
-
+                                   centerX: CONTROL_BUTTON_DIMENSION / 2,
+                                   centerY: CONTROL_BUTTON_DIMENSION / 2
+                                 } ) );
     openNode.addInputListener( {down: function() { open.set( true ); }} );
 
-    var closeNode = new Rectangle( CONTROL_BUTTON_INSET,
-                                   CONTROL_BUTTON_INSET,
-                                   CONTROL_BUTTON_DIMENSION,
-                                   CONTROL_BUTTON_DIMENSION,
-                                   3,
-                                   3,
-                                   {stroke: 'black', lineWidth: 1, fill: 'red', cursor: 'pointer'} );
+    var closeNode = new Rectangle( 0, 0, CONTROL_BUTTON_DIMENSION, CONTROL_BUTTON_DIMENSION, 3, 3,
+                                   {
+                                     cursor: 'pointer',
+                                     top: CONTROL_BUTTON_INSET,
+                                     left: CONTROL_BUTTON_INSET,
+                                     fill: new LinearGradient( 0, 0, CONTROL_BUTTON_DIMENSION, CONTROL_BUTTON_DIMENSION ).
+                                       addColorStop( 0, 'rgb(255, 26, 26 )' ).
+                                       addColorStop( 1, 'rgb(200, 0, 0 )' )
+                                   } );
     var minusShape = new Shape().moveTo( -CONTROL_BUTTON_SYMBOL_WIDTH / 2, 0 ).
       lineTo( CONTROL_BUTTON_SYMBOL_WIDTH / 2, 0 );
     closeNode.addChild( new Path( { shape: minusShape,
-                                   lineWidth: SYMBOL_LINE_WIDTH,
-                                   stroke: 'white',
-                                   center: closeNode.center } ) );
+                                    lineWidth: SYMBOL_LINE_WIDTH,
+                                    stroke: 'white',
+                                    centerX: CONTROL_BUTTON_DIMENSION / 2,
+                                    centerY: CONTROL_BUTTON_DIMENSION / 2
+                                  } ) );
     closeNode.addInputListener( {down: function() { open.set( false ); }} );
 
+    // Create the container that will hold the contents.
     var panelWidth = contentNode.width + 2 * CONTENT_INSET;
     var closedHeight = CONTROL_BUTTON_INSET * 2 + closeNode.height;
     var openHeight = CONTROL_BUTTON_INSET * 2 + openNode.height + 2 * CONTENT_INSET + contentNode.height;
