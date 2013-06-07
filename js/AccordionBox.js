b3221b75dca65c846efbcfc59c6214cf20b8c713
jbphet <john.blanco@colorado.edu>
2013-06-07 13:59:31 -0600

    Added plus and minus symbols to the accordion boxes.

--- js/AccordionBox.js
+++ js/AccordionBox.js
@@ -13,14 +13,18 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  var Path = require( 'SCENERY/nodes/Path' );
   var Text = require( 'SCENERY/nodes/Text' );
   var Vector2 = require( 'DOT/Vector2' );
   var Property = require( 'AXON/Property' );
+  var Shape = require( 'KITE/Shape' );
 
   // Constants
   var CONTROL_BUTTON_INSET = 4; // Can make this an option if desired.
-  var DEFAULT_CONTROL_BUTTON_DIMENSION = 20; // Can make this an option if desired.
+  var CONTROL_BUTTON_DIMENSION = 20; // Can make this an option if desired.
+  var CONTROL_BUTTON_SYMBOL_WIDTH = CONTROL_BUTTON_DIMENSION * 0.6;
   var CONTENT_INSET = 5; // Can make this an option if desired.
+  var SYMBOL_LINE_WIDTH = 3;
 
   /**
    * @param {Node} contentNode that will be vertically centered to the right of the button
@@ -45,20 +49,35 @@ define( function( require ) {
     // Create the open/close nodes.
     var openNode = new Rectangle( CONTROL_BUTTON_INSET,
                                   CONTROL_BUTTON_INSET,
-                                  DEFAULT_CONTROL_BUTTON_DIMENSION,
-                                  DEFAULT_CONTROL_BUTTON_DIMENSION,
+                                  CONTROL_BUTTON_DIMENSION,
+                                  CONTROL_BUTTON_DIMENSION,
                                   3,
                                   3,
                                   {stroke: 'black', lineWidth: 1, fill: 'green', cursor: 'pointer' } );
+    var plusShape = new Shape().moveTo( -CONTROL_BUTTON_SYMBOL_WIDTH / 2, 0 ).
+      lineTo( CONTROL_BUTTON_SYMBOL_WIDTH / 2, 0 ).
+      moveTo( 0, -CONTROL_BUTTON_SYMBOL_WIDTH / 2 ).
+      lineTo( 0, CONTROL_BUTTON_SYMBOL_WIDTH / 2 );
+    openNode.addChild( new Path( { shape: plusShape,
+                                   lineWidth: SYMBOL_LINE_WIDTH,
+                                   stroke: 'white',
+                                   center: openNode.center } ) );
+
     openNode.addInputListener( {down: function() { open.set( true ); }} );
 
     var closeNode = new Rectangle( CONTROL_BUTTON_INSET,
                                    CONTROL_BUTTON_INSET,
-                                   DEFAULT_CONTROL_BUTTON_DIMENSION,
-                                   DEFAULT_CONTROL_BUTTON_DIMENSION,
+                                   CONTROL_BUTTON_DIMENSION,
+                                   CONTROL_BUTTON_DIMENSION,
                                    3,
                                    3,
                                    {stroke: 'black', lineWidth: 1, fill: 'red', cursor: 'pointer'} );
+    var minusShape = new Shape().moveTo( -CONTROL_BUTTON_SYMBOL_WIDTH / 2, 0 ).
+      lineTo( CONTROL_BUTTON_SYMBOL_WIDTH / 2, 0 );
+    closeNode.addChild( new Path( { shape: minusShape,
+                                   lineWidth: SYMBOL_LINE_WIDTH,
+                                   stroke: 'white',
+                                   center: closeNode.center } ) );
     closeNode.addInputListener( {down: function() { open.set( false ); }} );
 
     var panelWidth = contentNode.width + 2 * CONTENT_INSET;
@@ -77,11 +96,11 @@ define( function( require ) {
     this.addChild( openContainer );
 
     var closedContainer = new Rectangle( 0, 0, panelWidth, closedHeight, 3, 3,
-                                       {
-                                         stroke: options.stroke,
-                                         lineWidth: options.lineWidth,
-                                         fill: options.fill
-                                       } );
+                                         {
+                                           stroke: options.stroke,
+                                           lineWidth: options.lineWidth,
+                                           fill: options.fill
+                                         } );
     closedContainer.addChild( openNode );
     this.addChild( closedContainer );
 
