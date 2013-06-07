2f88f3cb4f92b377ad6398dae698bec757167f64
jbphet <john.blanco@colorado.edu>
2013-06-07 12:56:56 -0600

    Intermediate checkin, should also pass lint at this point.

--- js/AccordionBox.js
+++ js/AccordionBox.js
@@ -8,11 +8,18 @@
 define( function( require ) {
   'use strict';
 
-  // imports
+  // Imports
   var ButtonListener = require( 'SCENERY/input/ButtonListener' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  var Text = require( 'SCENERY/nodes/Text' );
+  var Vector2 = require( 'DOT/Vector2' );
+
+  // Constants
+  var CONTROL_BUTTON_INSET = 4; // Can make this an option if desired.
+  var DEFAULT_CONTROL_BUTTON_DIMENSION = 20; // Can make this an option if desired.
+  var CONTENT_INSET = 5; // Can make this an option if desired.
 
   /**
    * @param {Node} contentNode that will be vertically centered to the right of the button
@@ -21,41 +28,50 @@ define( function( require ) {
    */
   function AccordionBox( contentNode, options ) {
 
-    options = _.extend( {
-      stroke: 'black' // color used to stroke the outer edge of the button
-    }, options );
+    options = _.extend( { // defaults
+                          stroke: 'black', // color used to stroke the outer edge of the button
+                          lineWidth: 1,
+                          fill: 'rgb( 238, 238, 238 )', // default background color
+                          font: "20px Tahoma"
+                        }, options );
 
     var thisNode = this;
-    Node.call( thisNode, options );
+    Node.call( this, options );
 
-    // nodes
-    var outerCircle = new Circle( options.radius, { fill: options.unselectedColor, stroke: options.stroke } );
-    var innerCircle = new Circle( options.radius / 3, { fill: options.centerColor } );
+    if ( options.openNode === undefined ) {
+      options.openNode = new Rectangle( CONTROL_BUTTON_INSET,
+                                        CONTROL_BUTTON_INSET,
+                                        DEFAULT_CONTROL_BUTTON_DIMENSION,
+                                        DEFAULT_CONTROL_BUTTON_DIMENSION,
+                                        3,
+                                        3,
+                                        {stroke: 'black', lineWidth: 1, fill: 'green'} );
+    }
+    if ( options.closeNode === undefined ) {
+      options.closeNode = new Rectangle( CONTROL_BUTTON_INSET,
+                                         CONTROL_BUTTON_INSET,
+                                         DEFAULT_CONTROL_BUTTON_DIMENSION,
+                                         DEFAULT_CONTROL_BUTTON_DIMENSION,
+                                         3,
+                                         3,
+                                         {stroke: 'black', lineWidth: 1, fill: 'red'} );
+    }
 
-    // rendering order
-    thisNode.addChild( outerCircle );
-    thisNode.addChild( innerCircle );
-    thisNode.addChild( contentNode );
+    var panelWidth = contentNode.width + 2 * CONTENT_INSET;
+    var closedHeight = CONTROL_BUTTON_INSET * 2 + options.closeNode.height;
+    var openHeight = CONTROL_BUTTON_INSET * 2 + options.closeNode.width + 2 * CONTENT_INSET + contentNode.height;
 
-    // layout
-    contentNode.left = outerCircle.right + options.xSpacing;
-    contentNode.centerY = outerCircle.centerY;
-
-    // add a "hit area" over the entire button, so we don't have a dead spot between button and node
-    thisNode.addChild( new Rectangle( thisNode.left, thisNode.top, thisNode.width, thisNode.height ) );
-
-    // sync control with model
-    property.addObserver( function( newValue ) {
-      outerCircle.fill = ( newValue === value ) ? options.selectedColor : options.unselectedColor;
-      innerCircle.visible = ( newValue === value );
-    } );
-
-    // set property value on 'up' event
-    thisNode.addInputListener( new ButtonListener( {
-      fire: function() {
-        property.set( value );
-      }
-    } ) );
+//    var box = new Rectangle( 0, 0, panelWidth, openHeight, 4, 4 );
+    var container = new Rectangle( 0, 0, panelWidth, openHeight, 3, 3,
+                                   {
+                                     stroke: options.stroke,
+                                     lineWidth: options.lineWidth,
+                                     fill: options.fill
+                                   } );
+    container.addChild( options.openNode );
+    contentNode.center = new Vector2( panelWidth / 2, openHeight - CONTENT_INSET - contentNode.height / 2 );
+    container.addChild( contentNode );
+    this.addChild( container );
   }
 
   inherit( Node, AccordionBox );
