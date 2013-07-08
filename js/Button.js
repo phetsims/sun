e9c2bb335626720b0dd55f8784014f5f66fcd321
Chris Malley <cmalley@pixelzoom.com>
2013-07-08 15:04:31 -0600

    remove ButtonModel, half of it was unused and it was complicating work on buttons.  Can be added later if needed, when we have a button type hierarchy.

--- js/Button.js
+++ js/Button.js
@@ -1,15 +1,17 @@
 // Copyright 2002-2013, University of Colorado Boulder
 
-//Render a simple button
-//TODO: Button.js is not ready for use in simulations, it will need further development & discussion first.
+/**
+ * Button with a rectangle around its content.
+ */
 define( function( require ) {
   "use strict";
 
-  var Node = require( 'SCENERY/nodes/Node' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
-  var ButtonModel = require( 'SUN/ButtonModel' );
+  // import
   var ButtonListener = require( 'SCENERY/input/ButtonListener' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Property = require( 'AXON/Property' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
 
   /**
    * @param {Node} content
@@ -22,7 +24,9 @@ define( function( require ) {
     options = _.extend( {
         cursor: 'pointer',
         fill: 'white',
+        disabledFill: 'rgb(175,175,175)',
         stroke: 'black',
+        disabledStroke: 'rgb(175,175,175)',
         lineWidth: 1,
         //TODO default margins should be computed based on content dimensions
         xMargin: 5,
@@ -35,43 +39,66 @@ define( function( require ) {
     var button = this;
     Node.call( button );
 
-    button.buttonModel = new ButtonModel();
-    button.buttonModel.listeners.push( callback );
+    button._enabled = new Property( true );
+    button._listeners = [ callback ];
 
-    var path = new Rectangle( 0, 0, content.width + ( 2 * options.xMargin ), content.height + ( 2 * options.yMargin ), options.cornerRadius, options.cornerRadius,
+    // rectangle around the content
+    var rectangle = new Rectangle( 0, 0, content.width + ( 2 * options.xMargin ), content.height + ( 2 * options.yMargin ), options.cornerRadius, options.cornerRadius,
       {stroke: options.stroke, lineWidth: options.lineWidth, fill: options.fill } );
-    button.addChild( path );
-    content.centerX = path.width / 2;
-    content.centerY = path.height / 2;
+    button.addChild( rectangle );
+
+    // content centered in the rectangle
+    content.centerX = rectangle.width / 2;
+    content.centerY = rectangle.height / 2;
     button.addChild( content );
+
     button.addInputListener( new ButtonListener( {
-      fire: function() {
-        button.buttonModel.fireListeners();
-      }
+      fire: function() { button._fire(); }
     } ) );
 
-    button.addPeer( '<input type="button" aria-label="' + _.escape( options.label ) + '">', {click: button.buttonModel.fireListeners.bind( button.buttonModel )} );
+    // accessibility
+    button.addPeer( '<input type="button" aria-label="' + _.escape( options.label ) + '">',
+      { click: button._fire.bind( button ) }
+    );
 
     //Mutate with the options after the layout is complete so that you can use width-dependent fields like centerX, etc.
     //TODO: Does this remove the need to put options in the super call above?
-    this.mutate( options );
+    button.mutate( options );
 
-    //TODO: Make it possible to enable/disable the content (if the content supports it)
-    //TODO: gray out the border
-    //TODO: Gray should be a bit brighter
-    //TODO: Non-pickable when disabled
-    button.buttonModel.enabledProperty.link( function( enabled ) {
-      path.fill = enabled ? options.fill : 'gray';
-      path.stroke = enabled ? options.stroke : 'gray';
+    button._enabled.link( function( enabled ) {
+      //TODO: enable/disable the content (if the content supports it)
+      rectangle.fill = enabled ? options.fill : options.disabledFill;
+      rectangle.stroke = enabled ? options.stroke : options.disabledStroke;
+      button.pickable = enabled;
     } );
   }
 
   inherit( Node, Button, {
+
+    // Adds a listener. If already a listener, this is a no-op.
     addListener: function( listener ) {
-      this.buttonModel.listeners.push( listener );
+      if ( this._listeners.indexOf( listener ) === -1 ) {
+        this._listeners.push( listener );
+      }
     },
-    set enabled( enabled ) { this.buttonModel.enabled = enabled; },
-    get enabled() { return this.buttonModel.enabled;}
+
+    // Remove a listener. If not a listener, this is a no-op.
+    removeListener: function( listener ) {
+      var i = this._listeners.indexOf( listener );
+      if ( i !== -1 ) {
+        this._listeners.splice( i, 1 );
+      }
+    },
+
+    _fire: function() {
+      this._listeners.forEach( function( listener ) {
+        listener();
+      } );
+    },
+
+    set enabled( enabled ) { this._enabled.set( enabled ); },
+
+    get enabled() { return this._enabled.get(); }
   } );
 
   return Button;
