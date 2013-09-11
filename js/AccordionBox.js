6bf408ac11ef6c1f6197e42d47a7e85ef7ec0dd1
jbphet <john.blanco@colorado.edu>
2013-09-11 14:59:41 -0700

    Moved ExpandCollapseButton into Sun from graphing-lines, integrated it into AccordionBox.

--- js/AccordionBox.js
+++ js/AccordionBox.js
@@ -9,29 +9,23 @@ define( function( require ) {
   'use strict';
 
   // Imports
-  var ButtonListener = require( 'SCENERY/input/ButtonListener' );
+  var ExpandCollapseButton = require( 'SUN/ExpandCollapseButton' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
-  var Path = require( 'SCENERY/nodes/Path' );
   var Text = require( 'SCENERY/nodes/Text' );
-  var Vector2 = require( 'DOT/Vector2' );
   var Property = require( 'AXON/Property' );
-  var Shape = require( 'KITE/Shape' );
-  var LinearGradient = require( 'SCENERY/util/LinearGradient' );
 
   // Constants
   var CONTROL_BUTTON_INSET = 4;
   var TITLE_INSET = 10;
   var CONTROL_BUTTON_DIMENSION = 16; // Can make this an option if desired.
-  var CONTROL_BUTTON_SYMBOL_WIDTH = CONTROL_BUTTON_DIMENSION * 0.6;
   var CONTENT_HORIZONTAL_INSET = 15; // Can make this an option if desired.
   var CONTENT_VERTICAL_INSET = 8; // Can make this an option if desired.
-  var SYMBOL_LINE_WIDTH = 3;
   var CORNER_ROUNDING = 3;
 
   /**
-   * @param {Node} contentNode that will be shown or hidden as the accordian
+   * @param {Node} contentNode that will be shown or hidden as the accordion
    * box is opened/closed.
    * @param {object} options Various key-value pairs that control the
    * appearance and behavior.  These are passed through to the Node super
@@ -62,39 +56,8 @@ define( function( require ) {
     // Create a property that tracks the open/closed state.
     this.open = new Property( options.initiallyOpen !== undefined ? options.initiallyOpen : true );
 
-    // Create the open/close control nodes.
-    var openNode = new Rectangle( 0, 0, CONTROL_BUTTON_DIMENSION, CONTROL_BUTTON_DIMENSION, 3, 3,
-      {
-        fill: new LinearGradient( 0, 0, CONTROL_BUTTON_DIMENSION, CONTROL_BUTTON_DIMENSION ).
-          addColorStop( 0, 'rgb(0, 230, 0 )' ).
-          addColorStop( 1, 'rgb(0, 179, 0 )' )
-      } );
-    var plusSymbolShape = new Shape().
-      moveTo( CONTROL_BUTTON_SYMBOL_WIDTH / 2, 0 ).
-      lineTo( CONTROL_BUTTON_SYMBOL_WIDTH / 2, CONTROL_BUTTON_SYMBOL_WIDTH ).
-      moveTo( 0, CONTROL_BUTTON_SYMBOL_WIDTH / 2 ).
-      lineTo( CONTROL_BUTTON_SYMBOL_WIDTH, CONTROL_BUTTON_SYMBOL_WIDTH / 2 );
-    openNode.addChild( new Path( plusSymbolShape, {
-      lineWidth: SYMBOL_LINE_WIDTH,
-      stroke: 'white',
-      centerX: CONTROL_BUTTON_DIMENSION / 2,
-      centerY: CONTROL_BUTTON_DIMENSION / 2
-    } ) );
-
-    var closeNode = new Rectangle( 0, 0, CONTROL_BUTTON_DIMENSION, CONTROL_BUTTON_DIMENSION, 3, 3,
-      {
-        fill: new LinearGradient( 0, 0, CONTROL_BUTTON_DIMENSION, CONTROL_BUTTON_DIMENSION ).
-          addColorStop( 0, 'rgb(255, 26, 26 )' ).
-          addColorStop( 1, 'rgb(200, 0, 0 )' )
-      } );
-    var minusSymbolShape = new Shape().moveTo( -CONTROL_BUTTON_SYMBOL_WIDTH / 2, 0 ).
-      lineTo( CONTROL_BUTTON_SYMBOL_WIDTH / 2, 0 );
-    closeNode.addChild( new Path( minusSymbolShape, {
-      lineWidth: SYMBOL_LINE_WIDTH,
-      stroke: 'white',
-      centerX: CONTROL_BUTTON_DIMENSION / 2,
-      centerY: CONTROL_BUTTON_DIMENSION / 2
-    } ) );
+    // Create the expand/collapse button.
+    var expandCollapseButton = new ExpandCollapseButton( CONTROL_BUTTON_DIMENSION, this.open );
 
     // Create the title, if present.
     var title = new Node();
@@ -116,7 +79,7 @@ define( function( require ) {
         lineWidth: options.lineWidth,
         fill: options.fill
       } );
-    openContainer.addChild( closeNode );
+    openContainer.addChild( expandCollapseButton );
     openContainer.addChild( contentNode );
     openContainer.addChild( title );
     this.addChild( openContainer );
@@ -128,7 +91,7 @@ define( function( require ) {
         lineWidth: options.lineWidth,
         fill: options.fill
       } );
-    closedContainer.addChild( openNode );
+    closedContainer.addChild( expandCollapseButton );
     closedContainer.addChild( title );
     this.addChild( closedContainer );
 
@@ -151,21 +114,18 @@ define( function( require ) {
     closedContainer.addChild( openCloseNode );
 
     // Lay out the contents of the containers.
-    openNode.top = CONTROL_BUTTON_INSET;
-    closeNode.top = CONTROL_BUTTON_INSET;
-    title.centerY = openNode.centerY;
+    expandCollapseButton.top = CONTROL_BUTTON_INSET;
+    title.centerY = expandCollapseButton.centerY;
     var titleLeftBound = TITLE_INSET;
     var titleRightBound = containerWidth - TITLE_INSET;
     contentNode.bottom = openContainerHeight - CONTENT_VERTICAL_INSET;
 
     if ( options.buttonPosition === 'left' ) {
-      openNode.left = CONTROL_BUTTON_INSET;
-      closeNode.left = CONTROL_BUTTON_INSET;
-      titleLeftBound = openNode.right + TITLE_INSET;
+      expandCollapseButton.left = CONTROL_BUTTON_INSET;
+      titleLeftBound = expandCollapseButton.right + TITLE_INSET;
     }
     else {
-      openNode.right = containerWidth - CONTROL_BUTTON_INSET;
-      closeNode.right = containerWidth - CONTROL_BUTTON_INSET;
+      expandCollapseButton.right = containerWidth - CONTROL_BUTTON_INSET;
       titleLeftBound = TITLE_INSET;
     }
 
---
+++ js/ExpandCollapseButton.js
@@ -0,0 +1,85 @@
+// Copyright 2002-2013, University of Colorado Boulder
+
+//TODO add button color change for 'over' and 'down'
+//TODO add enabled/disabled, probably via options.enabledProperty
+/**
+ * Button for expanding/collapsing something.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( function( require ) {
+  'use strict';
+
+  // imports
+  var ButtonListener = require( 'SCENERY/input/ButtonListener' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Path = require( 'SCENERY/nodes/Path' );
+  var Shape = require( 'KITE/Shape' );
+
+  /**
+   * @param {Number} sideLength length of one side of the square button
+   * @param {Property<Boolean>} expandedProperty
+   * @param {*} options
+   * @constructor
+   */
+  function ExpandCollapseButton( sideLength, expandedProperty, options ) {
+
+    options = _.extend( {}, options );
+
+    var thisButton = this;
+    Node.call( thisButton );
+
+    // configure the button shape
+    var cornerRadius = 0.1 * sideLength;
+    var buttonShape = Shape.roundRectangle( 0, 0, sideLength, sideLength, cornerRadius, cornerRadius );
+
+    // configure the +/- symbol on the button
+    var symbolLength = 0.6 * sideLength;
+    var symbolLineWidth = 0.15 * sideLength;
+    var symbolOptions = {
+      lineWidth: symbolLineWidth,
+      stroke: 'white',
+      centerX: sideLength / 2,
+      centerY: sideLength / 2
+    };
+
+    // Expand '+' button
+    var expandButton = new Path( buttonShape, { fill: 'rgb(0, 179, 0 )', stroke: 'black', lineWidth: 0.5 } );
+    var plusSymbolShape = new Shape()
+      .moveTo( symbolLength / 2, 0 )
+      .lineTo( symbolLength / 2, symbolLength )
+      .moveTo( 0, symbolLength / 2 )
+      .lineTo( symbolLength, symbolLength / 2 );
+    expandButton.addChild( new Path( plusSymbolShape, symbolOptions ) );
+
+    // Collapse '-' button
+    var collapseButton = new Path( buttonShape, { fill: 'rgb( 255, 85, 0 )', stroke: 'black', lineWidth: 0.5 } );
+    var minusSymbolShape = new Shape()
+      .moveTo( -symbolLength / 2, 0 )
+      .lineTo( symbolLength / 2, 0 );
+    collapseButton.addChild( new Path( minusSymbolShape, symbolOptions ) );
+
+    // rendering order
+    thisButton.addChild( expandButton );
+    thisButton.addChild( collapseButton );
+
+    // click to toggle
+    thisButton.cursor = 'pointer';
+    thisButton.addInputListener( new ButtonListener( {
+      fire: function() {
+        expandedProperty.set( !expandedProperty.get() );
+      }
+    } ) );
+
+    // sync with property
+    expandedProperty.link( function( expanded ) {
+      expandButton.visible = !expanded;
+      collapseButton.visible = expanded;
+    } );
+
+    thisButton.mutate( options );
+  }
+
+  return inherit( Node, ExpandCollapseButton );
+} );
