5a5da6f62d2b46cc65baa563b8810c31ea1b164a
Chris Malley <cmalley@pixelzoom.com>
2013-11-03 17:33:54 -0700

    closes #21

--- js/FontAwesomeNode.js
+++ js/FontAwesomeNode.js
@@ -8,18 +8,8 @@
 define( function( require ) {
   'use strict';
 
-  var Image = require( 'SCENERY/nodes/Image' );
-  var VBox = require( 'SCENERY/nodes/VBox' );
-  var DOM = require( 'SCENERY/nodes/DOM' );
-  var Node = require( 'SCENERY/nodes/Node' );
-  var HBox = require( 'SCENERY/nodes/HBox' );
-  var Scene = require( 'SCENERY/Scene' );
-  var Text = require( 'SCENERY/nodes/Text' );
   var Path = require( 'SCENERY/nodes/Path' );
-  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var Shape = require( 'KITE/Shape' );
-  var SimpleDragHandler = require( 'SCENERY/input/SimpleDragHandler' );
-  var Bounds2 = require( 'DOT/Bounds2' );
   var Matrix3 = require( 'DOT/Matrix3' );
   var inherit = require( 'PHET_CORE/inherit' );
 
