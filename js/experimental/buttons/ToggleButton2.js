0f52a50c48c95405491d673f645a0de1d7a8e964
jbphet <john.blanco@colorado.edu>
2014-03-19 16:39:07 -0700

    Removed support for a couple of features that are no longer needed.

--- js/experimental/buttons/ToggleButton2.js
+++ js/experimental/buttons/ToggleButton2.js
@@ -19,46 +19,12 @@ define( function( require ) {
       padY: 10,
       cursor: 'pointer',
       listener: function() { booleanProperty.value = !booleanProperty.value; },
-      accessibilityLabel: '',
-
-      // In 'radioButton' mode, pressing a toggle button repeatedly sets the
-      // value only to true.  Otherwise it sets it to true/false alternately.
-      // TODO: See note below about removing support for this mode.
-      radioButton: false
+      accessibilityLabel: ''
     }, options );
 
     var content = new ToggleNode( trueNode, falseNode, booleanProperty );
 
-    // TODO: Is this needed with the new version (new as of March 2014)?
-    if ( options.addRectangle ) {
-      this.path = new Rectangle( 0, 0, content.width + options.padX, content.height + options.padY, 10, 10, {stroke: 'black', lineWidth: 1, fill: '#e3e980'} );
-      this.addChild( this.path );
-      content.centerX = this.path.width / 2;
-      content.centerY = this.path.height / 2;
-    }
-
     RectangularPushButton.call( this, content, options );
-
-    // TODO: Revisit this and decide if we should just have a separate radio
-    // button.  For now, as of March 2014, support for radio button mode is
-    // being removed.
-    if ( options.radioButton ) { throw new Error( 'Radio button mode not supported by ToggleButton' ); }
-    /*
-     if ( options.radioButton ) {
-      this.addInputListener( {
-        up: function() {
-          booleanProperty.value = true;
-        }
-      } );
-    }
-    else {
-      this.addInputListener( {
-        up: function() {
-          booleanProperty.value = !booleanProperty.value;
-        }
-      } );
-    }
-     */
   }
 
   return inherit( RectangularPushButton, ToggleButton2 );
