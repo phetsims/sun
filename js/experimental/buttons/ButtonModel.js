fdec6100838321fe3bf17b8a3be3c4e7f515bad5
jbphet <john.blanco@colorado.edu>
2014-05-01 09:41:07 -0700

    Added descriptions for the basic properties of a button model, fixes #91.

--- js/experimental/buttons/ButtonModel.js
+++ js/experimental/buttons/ButtonModel.js
@@ -14,8 +14,19 @@ define( function( require ) {
   function ButtonModel() {
 
     PropertySet.call( this, {
+
+      // Property that tracks whether or not a pointer that could interact
+      // with the button is currently over the button.  For detailed semantics
+      // please see the descendant classes.
       over: false,
+
+      // Property that tracks whether a pointer is currently down on, a.k.a.
+      // pressing, the button.  For detailed semantics please see the
+      // descendant classes.
       down: false,
+
+      // Property that tracks whether or not the button is enabled.  For
+      // detailed semantics please see the descendant classes.
       enabled: true
     } );
   }
