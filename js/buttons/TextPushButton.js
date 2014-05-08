adf890a88f2149d63b29251995a09e3443ef9f08
jbphet <john.blanco@colorado.edu>
2014-05-08 10:38:16 -0700

    Created TextPushButton based on revised button infrastructure.

---
+++ js/buttons/TextPushButton.js
@@ -0,0 +1,33 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * Push button with text on a rectangle.
+ *
+ * @author John Blanco
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Font = require( 'SCENERY/util/Font' );
+  var RectangularPushButton = require( 'SUN/buttons/RectangularPushButton' );
+  var Text = require( 'SCENERY/nodes/Text' );
+  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
+
+  // constants
+  var DEFAULT_FONT = new PhetFont( 20 );
+
+  function TextPushButton( text, options ) {
+
+    options = _.extend( {
+      font: DEFAULT_FONT,
+      textFill: 'black'
+    }, options );
+
+    var textNode = new Text( text, { font: options.font, fill: options.textFill } );
+    RectangularPushButton.call( this, _.extend( { content: textNode }, options ) );
+  }
+
+  return inherit( RectangularPushButton, TextPushButton );
+} );
