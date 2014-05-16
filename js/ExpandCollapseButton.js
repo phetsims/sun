b19f3d822ebbef693cf0599a5572241fe6fe8b9d
Chris Malley <cmalley@pixelzoom.com>
2014-05-16 16:51:12 -0600

    #94 note that ExpandCollapseButton needs to be reimplemented using RectangularPushButton

--- js/ExpandCollapseButton.js
+++ js/ExpandCollapseButton.js
@@ -1,8 +1,6 @@
 // Copyright 2002-2013, University of Colorado Boulder
 
-//TODO reimplement as a RectangleButton subtype
-//TODO add button color change for 'over' and 'down'
-//TODO add enabled/disabled, probably via options.enabledProperty
+//TODO reimplement as a subtype of sun.buttons.RectangularPushButton
 /**
  * Button for expanding/collapsing something.
  *
