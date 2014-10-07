4ad11851063f00bc9f83af49b20f6c79cb5074c9
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-10-07 07:30:55 -0600

    add error checking to RBG contructor for duplicate values, see #124

--- js/buttons/RadioButtonGroup.js
+++ js/buttons/RadioButtonGroup.js
@@ -38,6 +38,25 @@ define( function( require ) {
       return obj.hasOwnProperty( 'node' ) && obj.hasOwnProperty( 'value' );
     } ), 'contentArray must be an array of objects with properties "node" and "value"' );
 
+    var i; // for loops
+
+    // make sure that each value passed into the contentArray is unique
+    var uniqueValues = [];
+    for ( i = 0; i < contentArray.length; i++ ) {
+      if ( uniqueValues.indexOf( contentArray[i].value ) < 0 ) {
+        uniqueValues.push( contentArray[i].value );
+      }
+      else {
+        throw new Error( 'Duplicate value: "' + contentArray[i].value + '" passed into RadioButtonGroup.js' );
+      }
+    }
+
+    // make sure that the property passed in currently has a value from the contentArray
+    if ( uniqueValues.indexOf( property.get() ) === -1 ) {
+      throw new Error( 'The property passed in to RadioButtonGroup has an illegal value "' + property.get() +
+                       '" that is not present in the contentArray' );
+    }
+
     options = _.extend( {
 
       //The distance between the radio buttons (as in VBox or HBox)
@@ -95,7 +114,7 @@ define( function( require ) {
 
     // make sure all radio buttons are the same size and create the RadioButtons
     var buttons = [];
-    var i, button, radioButton;
+    var button, radioButton;
     for ( i = 0; i < contentArray.length; i++ ) {
       options.xMargin = ( ( maxWidth - contentArray[i].node.width ) / 2 ) + options.buttonContentXMargin;
       options.yMargin = ( ( maxHeight - contentArray[i].node.height ) / 2 ) + options.buttonContentYMargin;
@@ -104,7 +123,7 @@ define( function( require ) {
       if ( contentArray[i].label ) {
         var labelOrientation = ( options.labelAlign === 'bottom' || options.labelAlign === 'top' ) ? 'vertical' : 'horizontal';
         var labelChildren = ( options.labelAlign === 'left' || options.labelAlign === 'top' ) ?
-          [contentArray[i].label, radioButton] : [radioButton, contentArray[i].label];
+                            [contentArray[i].label, radioButton] : [radioButton, contentArray[i].label];
         button = new LayoutBox( { children: labelChildren, spacing: options.labelSpacing, orientation: labelOrientation } );
       }
       else {
