3561295726835227cea2d17d07bb319a81e8a2b5
Chris Malley <cmalley@pixelzoom.com>
2015-04-09 11:16:24 -0600

    remove extra space from copyright comment on line 1 of .js files

--- js/buttons/RadioButtonGroupAppearance.js
+++ js/buttons/RadioButtonGroupAppearance.js
@@ -1,4 +1,4 @@
-//  Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2002-2014, University of Colorado Boulder
 
 /**
  * Defines the appearance of the radio buttons, see RadioButtonGroup.js. This file is designed to be used
