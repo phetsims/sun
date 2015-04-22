b570d5949543dae02d079b1893d11f421d3253f8
Sam Reid <reids@colorado.edu>
2015-04-22 10:57:32 -0600

    Instrumented RadioButton for tandem and dispose(), see https://github.com/phetsims/beers-law-lab/issues/98

--- js/RadioButton.js
+++ js/RadioButton.js
@@ -26,7 +26,8 @@ define( function( require ) {
 
     options = _.extend( {
       cursor: 'pointer',
-      focusable: true
+      focusable: true,
+      tandem: null
     }, options );
 
     var thisNode = this;
@@ -41,22 +42,39 @@ define( function( require ) {
     thisNode.addChild( deselectedNode );
 
     // sync control with model
-    property.link( function( newValue ) {
+    var syncWithModel = function( newValue ) {
       selectedNode.visible = ( newValue === value );
       deselectedNode.visible = !selectedNode.visible;
-    } );
+    };
+    property.link( syncWithModel );
 
     // set property value on fire
-    thisNode.addInputListener( new ButtonListener( {
+    var buttonListener = new ButtonListener( {
       fire: function() {
         thisNode.trigger1( 'startedCallbacksForFired', value );
         property.set( value );
         thisNode.trigger1( 'endedCallbacksForFired', value );
       }
-    } ) );
+    } );
+    thisNode.addInputListener( buttonListener );
 
     this.mutate( options );
+
+    options.tandem && options.tandem.addInstance( this );
+
+    this.disposeRadioButton = function() {
+      options.tandem && options.tandem.removeInstance( this );
+      thisNode.removeInputListener( buttonListener );
+      property.unlink( syncWithModel );
+    };
   }
 
-  return inherit( Node, RadioButton );
+  return inherit( Node, RadioButton, {
+
+      // Provide dispose() on the prototype for ease of subclassing.
+      dispose: function() {
+        this.disposeRadioButton();
+      }
+    }
+  );
 } );
