6aeef9d61a2b373fe51e466365b909cc3a749707
Chris Malley <cmalley@pixelzoom.com>
2015-06-30 12:32:28 -0600

    factor out CallbackTimer, used to implement fire-on-hold feature in PushButtonModel and FireOnHoldInputListener (formerly PressAndHoldInputListener) phetsims/scenery-phet#71

---
+++ js/CallbackTimer.js
@@ -0,0 +1,120 @@
+// Copyright 2002-2015, University of Colorado Boulder
+
+/**
+ * Timer that calls a set of registered callbacks.
+ * Utilizes joist.Timer, but provides a higher level of abstraction, hiding the details of managing the timer.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Timer = require( 'JOIST/Timer' );
+
+  /**
+   * @param {Object} [options]
+   * @constructor
+   */
+  function CallbackTimer( options ) {
+
+    options = _.extend( {
+      callback: null, // {function} convenience for adding 1 callback
+      delay: 400, // {number} start to fire continuously after pressing for this long (milliseconds)
+      interval: 100 // {number} fire continuously at this interval (milliseconds)
+    }, options );
+
+    // validate options
+    assert && assert( options.delay >= 0, 'bad value for delay: ' + options.delay );
+    assert && assert( options.interval > 0, 'bad value for interval: ' + options.interval );
+
+    this.delay = options.delay; // @private
+    this.interval = options.interval; // @private
+
+    this.callbacks = []; // @private
+    if ( options.callback ) { this.callbacks.push( options.callback ); }
+
+    this.delayID = null; // @private identifier for timer associated with the initial delay
+    this.intervalID = null; // @private identifier for timer associates with the continuous interval
+    this.fired = false;  // @private has the timer fired since it was started?
+  }
+
+  return inherit( Object, CallbackTimer, {
+
+    // @public Is the timer running?
+    isRunning: function() {
+      return ( this.delayID !== null || this.intervalID !== null );
+    },
+
+    // @public Starts the timer.
+    start: function() {
+      var self = this;
+      if ( !self.isRunning() ) {
+        self.fired = false;
+        self.delayID = Timer.setTimeout( function() {
+          self.delayID = null;
+          self.fire();
+          self.intervalID = Timer.setInterval( function() {
+            self.fire();
+          }, self.interval );
+        }, self.delay );
+      }
+    },
+
+    /**
+     * Stops the timer.
+     * @param {boolean} fire - should we fire if we haven't fired already?
+     * @public
+     */
+    stop: function( fire ) {
+      if ( this.isRunning() ) {
+        if ( this.delayID ) {
+          Timer.clearTimeout( this.delayID );
+          this.delayID = null;
+        }
+        if ( this.intervalID ) {
+          Timer.clearInterval( this.intervalID );
+          this.intervalID = null;
+        }
+        if ( fire && !this.fired ) {
+          this.fire();
+        }
+      }
+    },
+
+    // @public Adds a {function} callback.
+    addCallback: function( callback ) {
+      if ( this.callbacks.indexOf( callback ) === -1 ) {
+        this.callbacks.push( callback );
+      }
+    },
+
+    // @public Removes a {function} callback.
+    removeCallback: function( callback ) {
+      var index = this.callbacks.indexOf( callback );
+      if ( index !== -1 ) {
+        this.callbacks.splice( index, 1 );
+      }
+    },
+
+    /**
+     * Calls all callbacks.
+     * Clients are free to call this when the timer is not running.
+     * @public
+     */
+    fire: function() {
+      assert && assert( !this.isRunning() );
+      var callbacksCopy = this.callbacks.slice( 0 );
+      for ( var i = 0; i < callbacksCopy.length; i++ ) {
+        callbacksCopy[ i ]();
+      }
+      this.fired = true;
+    },
+
+    dispose: function() {
+      this.stop();
+      this.callbacks.length = 0;
+    }
+  } );
+} );
--- js/buttons/PushButtonModel.js
+++ js/buttons/PushButtonModel.js
@@ -5,108 +5,109 @@
  *
  * @author Sam Reid
  * @author John Blanco
+ * @author Chris Malley (PixelZoom, Inc.)
  */
 define( function( require ) {
   'use strict';
 
   // modules
   var ButtonModel = require( 'SUN/buttons/ButtonModel' );
+  var CallbackTimer = require( 'SUN/CallbackTimer' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var Timer = require( 'JOIST/Timer' );
 
   /**
    * @param {Object} [options]
    * @constructor
    */
   function PushButtonModel( options ) {
-    var pushButtonModel = this;
 
     options = _.extend( {
-      fireOnDown: false, // true: fire on pointer down; false: fire on pointer up if pointer is over button
-      listener: null,
 
-      // options related to fire-on-hold feature
-      fireOnHold: false,
+      fireOnDown: false, // true: fire on pointer down; false: fire on pointer up if pointer is over button
+      listener: null, // {function} convenience for adding 1 listener
+
+      // fire-on-hold feature
+      fireOnHold: false, // is the fire-on-hold feature enabled?
       fireOnHoldDelay: 400, // start to fire continuously after pressing for this long (milliseconds)
       fireOnHoldInterval: 100 // fire continuously at this interval (milliseconds)
     }, options );
 
-    // check that the timer values are reasonable if fire-on-hold is turned on
-    assert && assert( !options.fireOnHold || ( options.fireOnHoldDelay && options.fireOnHoldInterval ) );
-
+    var pushButtonModel = this;
     ButtonModel.call( this );
 
-    this.listeners = []; //@private
+    this.listeners = []; // @private
     if ( options.listener !== null ) {
       this.listeners.push( options.listener );
     }
 
-    var delayID = null; // identified for timer associated with the initial delay
-    var intervalID = null; // identifier for timer associates with the continuous interval
-    var firedByHold = false; // flag for preventing extra firing on button up after holding
+    // Create a timer to handle the optional fire-on-hold feature.
+    // When that feature is enabled, calling this.fire is delegated to the timer.
+    if ( options.fireOnHold ) {
+      this.timer = new CallbackTimer( {
+        callback: this.fire.bind( this ),
+        delay: options.fireOnHoldDelay,
+        interval: options.fireOnHoldInterval
+      } );
+    }
 
-    // cleans up the timers
-    var cleanupTimer = function() {
-      if ( delayID ) {
-        Timer.clearTimeout( delayID );
-        delayID = null;
-      }
-      if ( intervalID ) {
-        Timer.clearInterval( intervalID );
-        intervalID = null;
-      }
-    };
-
-    // Handle changes of the 'down' property to true.
+    // Point down
     this.property( 'down' ).onValue( true, function() {
       if ( pushButtonModel.enabled ) {
-
         if ( options.fireOnDown ) {
           pushButtonModel.fire();
         }
-
-        // make sure timers are in the expected state
-        assert && assert( delayID === null && intervalID === null, 'Timers non-null on button down' );
-
-        if ( options.fireOnHold ){
-          // set timers for press-and-hold behavior
-          delayID = Timer.setTimeout( function() {
-            pushButtonModel.fire();
-            firedByHold = true;
-            delayID = null;
-            intervalID = Timer.setInterval( function() {
-              pushButtonModel.fire();
-            }, options.fireOnHoldInterval );
-          }, options.fireOnHoldDelay );
+        if ( pushButtonModel.timer ) {
+          pushButtonModel.timer.start();
         }
       }
     } );
 
-    // If button was released and "fire on down" was not set, fire the listeners
+    // Pointer up
     this.property( 'down' ).onValue( false, function() {
-      cleanupTimer();
-      if ( !options.fireOnDown && !firedByHold && pushButtonModel.over && pushButtonModel.enabled ) {
+      var fire = ( !options.fireOnDown && pushButtonModel.over && pushButtonModel.enabled ); // should the button fire?
+      if ( pushButtonModel.timer ) {
+        pushButtonModel.timer.stop( fire );
+      }
+      else if ( fire ) {
         pushButtonModel.fire();
       }
-      firedByHold = false;
+    } );
+
+    // Stop the timer when the button is disabled.
+    this.property( 'enabled' ).link( function( enabled ) {
+      if ( !enabled && pushButtonModel.timer ) {
+        pushButtonModel.timer.stop( false ); // Stop the timer, don't fire if we haven't already
+      }
     } );
   }
 
   return inherit( ButtonModel, PushButtonModel, {
 
     dispose: function() {
-      this.listeners.length = 0;
       ButtonModel.prototype.dispose.call( this );
+      this.listeners.length = 0;
+      if ( this.timer ) {
+        this.timer.dispose();
+        this.timer = null;
+      }
     },
 
-    // Adds a listener. If already a listener, this is a no-op.
+    /**
+     * Adds a listener. If already a listener, this is a no-op.
+     * @param {function} listener
+     * @public
+     */
     addListener: function( listener ) {
       if ( this.listeners.indexOf( listener ) === -1 ) {
         this.listeners.push( listener );
       }
     },
 
-    // Remove a listener. If not a listener, this is a no-op.
+    /**
+     * Removes a listener. If not a listener, this is a no-op.
+     * @param {function} listener
+     * @public
+     */
     removeListener: function( listener ) {
       var i = this.listeners.indexOf( listener );
       if ( i !== -1 ) {
@@ -114,8 +115,10 @@ define( function( require ) {
       }
     },
 
-    // Fires all listeners.
-    // @private with the possible exception of hooking up for accessibility.
+    /**
+     * Fires all listeners.
+     * @private with the possible exception of hooking up for accessibility
+     */
     fire: function() {
       this.trigger0( 'startedCallbacksForFired' );
       var copy = this.listeners.slice( 0 );
