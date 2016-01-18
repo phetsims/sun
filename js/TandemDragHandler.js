e7a5013dbc9b8ef3886e4276f491d3e85e9d4408
samreid <reids@colorado.edu>
2016-01-18 09:44:10 -0700

    Addressed review comments, see https://github.com/phetsims/together/issues/254

--- js/TandemDragHandler.js
+++ js/TandemDragHandler.js
@@ -15,11 +15,16 @@ define( function( require ) {
   var Brand = require( 'BRAND/Brand' );
 
   /**
-   * Even though the tandem is required, it is passed via options for compatibility with SimpleDragHandler
-   * @param options
+   * @param {Object} [options]
    * @constructor
    */
   function TandemDragHandler( options ) {
+
+    options = _.extend( {
+      tandem: null
+
+      /* Please see additional options described in the super type*/
+    }, options );
 
     // Generate all emitters in every case to minimize the number of hidden classes,
     // see http://www.html5rocks.com/en/tutorials/speed/v8/
@@ -32,43 +37,40 @@ define( function( require ) {
     this.startedCallbacksForDragEndedEmitter = new Emitter(); // @public (together)
     this.endedCallbacksForDragEndedEmitter = new Emitter(); // @public (together)
 
+    var optionsCopy = _.clone( options );
+
     // For non-phet-io brands, skip tandem callbacks to save CPU
     if ( Brand.id === 'phet-io' ) {
       var tandemDragHandler = this;
 
-      var newOptions = _.extend( {}, options );
-
       // Replace start/end/drag even if they did not exist, to get the tracking.
-      newOptions.start = function( event, trail ) {
+      optionsCopy.start = function( event, trail ) {
         tandemDragHandler.startedCallbacksForDragStartedEmitter.emit2( event.pointer.point.x, event.pointer.point.y );
         options.start && options.start( event, trail );
         tandemDragHandler.endedCallbacksForDragStartedEmitter.emit();
       };
 
-      newOptions.drag = function( event, trail ) {
+      optionsCopy.drag = function( event, trail ) {
         tandemDragHandler.startedCallbacksForDraggedEmitter.emit2( event.pointer.point.x, event.pointer.point.y );
         options.drag && options.drag( event, trail );
         tandemDragHandler.endedCallbacksForDraggedEmitter.emit();
       };
 
-      newOptions.end = function( event, trail ) {
+      optionsCopy.end = function( event, trail ) {
         tandemDragHandler.startedCallbacksForDragEndedEmitter.emit2( event.pointer.point.x, event.pointer.point.y );
         options.end && options.end( event, trail );
         tandemDragHandler.endedCallbacksForDragEndedEmitter.emit();
       };
-      SimpleDragHandler.call( this, newOptions );
-
-      options.tandem && options.tandem.addInstance( this );
-
-      // @private
-      this.disposeTandemDragHandler = function() {
-        options.tandem && options.tandem.removeInstance( tandemDragHandler );
-      };
     }
-    else {
-      SimpleDragHandler.call( this, options );
-      this.disposeTandemDragHandler = function() {}; // nothing to clean up in this case
-    }
+
+    SimpleDragHandler.call( this, optionsCopy );
+
+    options.tandem && options.tandem.addInstance( this );
+
+    // @private
+    this.disposeTandemDragHandler = function() {
+      options.tandem && options.tandem.removeInstance( tandemDragHandler );
+    };
   }
 
   sun.register( 'TandemDragHandler', TandemDragHandler );
