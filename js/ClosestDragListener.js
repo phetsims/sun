36a0486f740a248ee0a7d1b2a08e3c225c323c6f
Jonathan Olson <olsonsjc@gmail.com>
2016-01-29 19:01:54 -0700

    Adding initial version of code support for dragging closest object. See https://github.com/phetsims/pendulum-lab/issues/117

---
+++ js/ClosestDragListener.js
@@ -0,0 +1,98 @@
+// Copyright 2013-2015, University of Colorado Boulder
+
+/**
+ * A Scenery input listener that is able to find the closest in a list of nodes to a "down" event and trigger a drag
+ * on the closest (assuming it has a SimpleDragHandler).
+ *
+ * Handles items of the form:
+ * {
+ *   startDrag: function( event ),
+ *   computeDistance: function( globalPoint ) : number
+ * }
+ *
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var sun = require( 'SUN/sun' );
+
+  /**
+   * @constructor
+   *
+   * @param {number} touchThreshold - The distance
+   */
+  function ClosestDragListener( touchThreshold, mouseThreshold ) {
+    // @private
+    this.touchThreshold = touchThreshold;
+    this.mouseThreshold = mouseThreshold;
+
+    // @private
+    this.items = [];
+  }
+
+  sun.register( 'ClosestDragListener', ClosestDragListener );
+
+  inherit( Object, ClosestDragListener, {
+    /**
+     * Adds an item that can be dragged.
+     * @public
+     *
+     * @param {item} item
+     */
+    addDraggableItem: function( item ) {
+      assert && assert( !!item.startDrag && !!item.computeDistance, 'Added an invalid item for ClosestDragListener' );
+      this.items.push( item );
+    },
+
+    /**
+     * Removes a previously-added item.
+     * @public
+     *
+     * @param {item} item
+     */
+    removeDraggableItem: function( item ) {
+      var index = _.indexOf( this.items, item );
+      assert && assert( index >= 0 );
+      this.items.splice( index, 1 );
+    },
+
+    down: function( event ) {
+      // If there was nothing else in the way
+      if ( event.target === event.currentTarget ) {
+        var threshold = 0;
+        if ( event.pointer.isTouch ) {
+          threshold = this.touchThreshold;
+        }
+        if ( event.pointer.isMouse ) {
+          threshold = this.mouseThreshold;
+        }
+        if ( threshold ) {
+          // search for the closest item
+          var currentItem = null;
+          var currentDistance = Number.POSITIVE_INFINITY;
+          var globalPoint = event.pointer.point;
+          var numItems = this.items.length;
+          for ( var i = 0; i < numItems; i++ ) {
+            var item = this.items[ i ];
+
+            var distance = item.computeDistance( globalPoint );
+            if ( distance < currentDistance ) {
+              currentDistance = distance;
+              currentItem = item;
+            }
+          }
+
+          // if we have a closest item under the threshold, attempt to start a drag on it
+          if ( currentItem && currentDistance < threshold ) {
+            currentItem.startDrag( event );
+          }
+        }
+      }
+    }
+  } );
+
+  return ClosestDragListener;
+} );
