c390c96ae2851cc1bde427a5060b9ecf26f4e280
Andrew Adare <andrewadare@gmail.com>
2016-07-20 09:17:46 -0600

    Add THSliderTrack (#489)

---
+++ js/types/sun/THSliderTrack.js
@@ -0,0 +1,31 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Andrew Adare
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var assertInstanceOf = require( 'PHET_IO/assertions/assertInstanceOf' );
+  var phetioInherit = require( 'PHET_IO/phetioInherit' );
+  var phetioNamespace = require( 'PHET_IO/phetioNamespace' );
+  var TNode = require( 'PHET_IO/types/scenery/nodes/TNode' );
+  var TTandemDragHandler = require( 'PHET_IO/types/tandem/scenery/input/TTandemDragHandler' );
+
+  var THSliderTrack = phetioInherit( TNode, 'THSliderTrack', function( sliderTrack, phetioID ) {
+    TNode.call( this, sliderTrack, phetioID );
+    assertInstanceOf( sliderTrack, phet.sun.HSliderTrack );
+  }, {}, {
+    api: {
+      trackInputListener: TTandemDragHandler
+    },
+    documentation: 'The track for a knob of a traditional slider'
+  } );
+
+  phetioNamespace.register( 'THSliderTrack', THSliderTrack );
+
+  return THSliderTrack;
+} );
