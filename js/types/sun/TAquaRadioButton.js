40c4cc60751869d540d84ccb5c1849f1f24173e8
samreid <reids@colorado.edu>
2016-09-02 12:30:22 -0600

    Refactored and re-instrumented AquaRadioButton, see https://github.com/phetsims/sun/issues/258

---
+++ js/types/sun/TAquaRadioButton.js
@@ -0,0 +1,43 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ * PhET-iO Adapter for sun's AquaRadioButton
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Andrew Adare
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var assertInstanceOfTypes = require( 'PHET_IO/assertions/assertInstanceOfTypes' );
+  var phetioInherit = require( 'PHET_IO/phetioInherit' );
+  var TVoid = require( 'PHET_IO/types/TVoid' );
+  var TBoolean = require( 'PHET_IO/types/TBoolean' );
+  var phetioNamespace = require( 'PHET_IO/phetioNamespace' );
+  var TRadioButton = require( 'PHET_IO/types/sun/buttons/TRadioButton' );
+
+  var TAquaRadioButton = function( valueType ) {
+    assert && assert( !!valueType, 'valueType must be defined' );
+    return phetioInherit( TRadioButton( valueType ), 'TAquaRadioButton', function TAquaRadioButtonImpl( radioButton, phetioID ) {
+      assertInstanceOfTypes( radioButton, [ phet.sun.AquaRadioButton ] );
+      TRadioButton( valueType ).call( this, radioButton, phetioID );
+    }, {
+      setCircleButtonVisible: {
+        returnType: TVoid,
+        parameterTypes: [ TBoolean ],
+        implementation: function( visible ) {
+          this.instance.setCircleButtonVisible( visible );
+        },
+        documentation: 'Sets whether the circular part of the radio button will be displayed.'
+      }
+    }, {
+      documentation: 'A radio button which looks like the Mac "Aqua" radio buttons',
+      events: TRadioButton( valueType ).events // TODO: Is this automatically inherited from the parent?
+    } );
+  };
+
+  phetioNamespace.register( 'TAquaRadioButton', TAquaRadioButton );
+
+  return TAquaRadioButton;
+} );
