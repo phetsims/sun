41b1ea18212efdb087f839b7fab4c4326b7db5b7
Jesse Greenberg <jesse.greenberg@colorado.edu>
2017-01-31 11:20:11 -0700

    Remove AccessiblePeer from RectangularPushButton, see phetsims/scenery#596

--- js/buttons/RectangularPushButton.js
+++ js/buttons/RectangularPushButton.js
@@ -12,7 +12,6 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var AccessiblePeer = require( 'SCENERY/accessibility/AccessiblePeer' );
   var inherit = require( 'PHET_CORE/inherit' );
   var PushButtonInteractionStateProperty = require( 'SUN/buttons/PushButtonInteractionStateProperty' );
   var PushButtonModel = require( 'SUN/buttons/PushButtonModel' );
@@ -30,13 +29,7 @@ define( function( require ) {
   function RectangularPushButton( options ) {
 
     options = _.extend( {
-      accessibleLabel: '', // {string} invisible label for a11y
-      tandem: Tandem.tandemRequired(), // {Tandem|null}
-      accessibleContent: {
-        createPeer: function( accessibleInstance ) {
-          return new RectangularPushButtonAccessiblePeer( accessibleInstance, options.accessibleLabel, options.listener );
-        }
-      }
+      tandem: Tandem.tandemRequired() // {Tandem|null}
     }, options );
 
     // Safe to pass through options to the PushButtonModel like "fireOnDown".  Other scenery options will be safely ignored.
@@ -51,7 +44,9 @@ define( function( require ) {
     };
   }
 
-  inherit( RectangularButtonView, RectangularPushButton, {
+  sun.register( 'RectangularPushButton', RectangularPushButton );
+
+  return inherit( RectangularButtonView, RectangularPushButton, {
 
       // @public
       dispose: function() {
@@ -69,66 +64,6 @@ define( function( require ) {
       removeListener: function( listener ) {
         this.buttonModel.removeListener( listener );
       }
-    },
-
-    // statics
-    {
-      /**
-       * Extend the accessible peer of RectangularPushButton to add custom accessibility attributes in subtypes.
-       *
-       * @param {AccessibleInstance} accessibleInstance
-       * @param {string} buttonLabel
-       * @param {function} listener
-       * @returns {ScreenViewAccessiblePeer}
-       * @constructor
-       * @public
-       */
-      RectangularPushButtonAccessiblePeer: function( accessibleInstance, buttonLabel, listener ) {
-        return new RectangularPushButtonAccessiblePeer( accessibleInstance, buttonLabel, listener );
-      }
     }
   );
-
-  sun.register( 'RectangularPushButton', RectangularPushButton );
-
-  /**
-   * Create the accessible peer which represents the RectangularPushButton in the parallel DOM.
-   *
-   * @param {AccessibleInstance} accessibleInstance
-   * @param {string} buttonLabel - invisible label for a11y
-   * @param {function} listener - the listener function called on press for this RectangularPushButton
-   * @constructor
-   * @private
-   */
-  function RectangularPushButtonAccessiblePeer( accessibleInstance, buttonLabel, listener ) {
-    this.initialize( accessibleInstance, buttonLabel, listener );
-  }
-
-  inherit( AccessiblePeer, RectangularPushButtonAccessiblePeer, {
-
-    /**
-     * Initialized dom elements and its attributes for the screen view in the parallel DOM.
-     *
-     * @param {AccessibleInstance} accessibleInstance
-     * @param {string} [buttonLabel] - invisible label for a11y
-     * @param {function} listener - the listener function called on press for this RectangularPushButton
-     * @public (a11y)
-     */
-    initialize: function( accessibleInstance, buttonLabel, listener ) {
-
-      // will look like <button aria-label="Button Label" type="button">
-      var domElement = document.createElement( 'button' );
-      domElement.textContent = buttonLabel;
-
-      // fire on click event
-      domElement.addEventListener( 'click', function() {
-        listener();
-      } );
-
-      this.initializeAccessiblePeer( accessibleInstance, domElement );
-
-    }
-
-  } );
-  return RectangularPushButton;
 } );
