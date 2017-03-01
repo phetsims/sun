48a0cc481c22d717fc1d699b74d09edbda454e40
jbphet <john.blanco@colorado.edu>
2017-02-28 17:26:25 -0700

    did initial instrumentation of AccordionBox for phet-io, see https://github.com/phetsims/build-an-atom/issues/107

---
+++ js/types/sun/TAccordionBox.js
@@ -0,0 +1,55 @@
+// Copyright 2017, University of Colorado Boulder
+
+/**
+ * wrapper type for AccordionBox
+ * @author John Blanco
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var assertInstanceOf = require( 'PHET_IO/assertions/assertInstanceOf' );
+  var phetioInherit = require( 'PHET_IO/phetioInherit' );
+  var phetioNamespace = require( 'PHET_IO/phetioNamespace' );
+  var TNode = require( 'PHET_IO/types/scenery/nodes/TNode' );
+  var toEventOnEmit = require( 'PHET_IO/events/toEventOnEmit' );
+
+  /**
+   * Wrapper type for phet/sun's AccordionBox class.
+   * @param accordionBox
+   * @param phetioID
+   * @constructor
+   */
+  function TAccordionBox( accordionBox, phetioID ) {
+    assertInstanceOf( accordionBox, phet.sun.AccordionBox );
+    TNode.call( this, accordionBox, phetioID );
+
+    toEventOnEmit(
+      accordionBox.startedCallbacksForCollapsedTitleBarDownEmitter,
+      accordionBox.endedCallbacksForCollapsedTitleBarDownEmitter,
+      'user',
+      phetioID,
+      TAccordionBox,
+      'opened'
+    );
+
+    toEventOnEmit(
+      accordionBox.startedCallbacksForExpandedTitleBarDownEmitter,
+      accordionBox.endedCallbacksForExpandedTitleBarDownEmitter,
+      'user',
+      phetioID,
+      TAccordionBox,
+      'closed'
+    );
+  }
+
+  phetioInherit( TNode, 'TAccordionBox', TAccordionBox, {}, {
+    documentation: 'A traditional accordionBox',
+    events: [ 'opened', 'closed' ]
+  } );
+
+  phetioNamespace.register( 'TAccordionBox', TAccordionBox );
+
+  return TAccordionBox;
+} );
+
