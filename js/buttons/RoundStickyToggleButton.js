04b689bc2f76ba9ea9c4c72f733f34730a3a937f
Jesse Greenberg <jesse.greenberg@colorado.edu>
2018-04-17 17:44:35 -0600

    update usage of addAccessibleInputListener, see phetsims/scenery#764

--- js/buttons/RoundStickyToggleButton.js
+++ js/buttons/RoundStickyToggleButton.js
@@ -46,11 +46,12 @@ define( function( require ) {
     RoundButtonView.call( this, this.toggleButtonModel, new StickyToggleButtonInteractionStateProperty( this.toggleButtonModel ), options );
 
     // @private (a11y) - toggle the button when we receive the accessible click event
-    this.accessibleClickListener = this.addAccessibleInputListener( {
+    this.accessibleClickListener = {
       click: function( event ) {
         self.toggleButtonModel.toggle();
       }
-    } );
+    };
+    this.addAccessibleInputListener( this.accessibleClickListener );
   }
 
   sun.register( 'RoundStickyToggleButton', RoundStickyToggleButton );
