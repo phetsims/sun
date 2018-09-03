c32bdce9a2ed2537ca0cb0dde8f5d662b1915999
Chris Malley <cmalley@pixelzoom.com>
2018-09-03 12:42:58 -0600

    Revert "fix assertion message, https://github.com/phetsims/sun/issues/380"
    This reverts commit d3819e5309208f64a17e8a8de112e4b21a60d035.

--- js/Slider.js
+++ js/Slider.js
@@ -107,7 +107,7 @@ define( function( require ) {
     this.orientation = options.orientation; // @private
 
     assert && assert( options.rotation === undefined,
-      'Slider specifies rotation based on orientation' );
+      'Slider specified rotation based on orientation' );
     if ( options.orientation === 'vertical' ) {
       options.rotation = VERTICAL_ROTATION;
     }
