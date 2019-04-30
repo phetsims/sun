4149bfae734ef72bef1bbb5f2629e6fc174cfd8d
Chris Malley <cmalley@pixelzoom.com>
2019-04-29 19:57:55 -0600

    use _.includes, https://github.com/phetsims/tasks/issues/998

--- js/ComboBoxButton.js
+++ js/ComboBoxButton.js
@@ -59,9 +59,9 @@ define( require => {
 
       }, options );
 
-      assert && assert( ALIGN_VALUES.includes( options.align ),
+      assert && assert( _.includes( ALIGN_VALUES, options.align ),
         'invalid align: ' + options.align );
-      assert && assert( ARROW_DIRECTION_VALUES.includes( options.arrowDirection ),
+      assert && assert( _.includes( ARROW_DIRECTION_VALUES, options.arrowDirection ),
         'invalid arrowDirection: ' + options.arrowDirection );
 
       // To improve readability
