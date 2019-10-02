fbe9d6c70e7967bd83c80af53cccdd283b731f22
zepumph <michael.kauzmann@colorado.edu>
2019-10-01 16:55:58 -0800

    remove a11yDecimalPlaces, have step rely on Util.numberOfDecimalPlaces instead, https://github.com/phetsims/sun/issues/530

--- js/accessibility/AccessibleValueHandler.js
+++ js/accessibility/AccessibleValueHandler.js
@@ -62,7 +62,7 @@ define( require => {
          */
         initializeAccessibleValueHandler( valueProperty, rangeProperty, enabledProperty, options ) {
 
-          // if rounding to keyboard step, keyboardStep must be defined so values aren't skipped and the slider   
+          // if rounding to keyboard step, keyboardStep must be defined so values aren't skipped and the slider
           // doesn't get stuck while rounding to the nearest value, see https://github.com/phetsims/sun/issues/410
           if ( assert && options.roundToStepSize ) {
             assert( options.keyboardStep, 'rounding to keyboardStep, define appropriate keyboardStep to round to' );
@@ -83,7 +83,7 @@ define( require => {
             // TODO: this should be an enumeration, https://github.com/phetsims/gravity-force-lab-basics/issues/134
             ariaOrientation: 'horizontal', // specify orientation, read by assistive technology
 
-            a11yDecimalPlaces: 0, // number of decimal places for the value when formatted and read by assistive technology
+            a11yValuePattern: SunConstants.VALUE_NAMED_PLACEHOLDER, // {string} if you want units or additional content, add to pattern
 
             // {boolean} - Whether or not to round the value to a multiple of the keyboardStep. This will only round
             // the value on normal key presses, rounding will not occur on large jumps like page up/page down/home/end.
@@ -193,7 +193,7 @@ define( require => {
           // initialize slider attributes
           this.ariaOrientation = options.ariaOrientation;
 
-          // @private - track previous values for callbacks outside of Property listeners 
+          // @private - track previous values for callbacks outside of Property listeners
           this.oldValue = null;
 
           // @public - Emitted whenever there is an attempt to change the value in a particular direction. Note that
@@ -231,9 +231,6 @@ define( require => {
 
           // @private - setting to enable/disable rounding to the step size
           this.roundToStepSize = options.roundToStepSize;
-
-          // @public (read-only) - precision for the output value to be read by an assistive device
-          this.a11yDecimalPlaces = options.a11yDecimalPlaces;
 
           // @private {function}
           this.a11yMapValue = options.a11yMapValue;
@@ -382,8 +379,7 @@ define( require => {
           const mappedValue = this.a11yMapValue( this._valueProperty.value );
           assert && assert( typeof mappedValue === 'number', 'a11yMapValue must return a number' );
 
-          // format the value text for reading
-          return Util.toFixedNumber( mappedValue, this.a11yDecimalPlaces );
+          return mappedValue;
         },
 
         /**
@@ -793,14 +789,15 @@ define( require => {
          * @private
          */
         updateSiblingStepAttribute() {
-          let stepValue = Math.pow( 10, -this.a11yDecimalPlaces );
+          const smallestStep = Math.min( Math.min( this.keyboardStep, this.shiftKeyboardStep ), this.pageKeyboardStep );
+          let stepValue = Math.pow( 10, -Util.numberOfDecimalPlaces( smallestStep ) );
 
           const fullRange = this._rangeProperty.get().getLength();
 
           // step is too small relative to full range for VoiceOver to receive input, fall back to portion of
           // full range
           if ( stepValue / fullRange < 1e-5 ) {
-            stepValue = this._rangeProperty.get().max / 100;
+            stepValue = fullRange / 100;
           }
 
           this.setAccessibleAttribute( 'step', stepValue );
