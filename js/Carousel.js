24ec050eb66a7d671983ae846179f5b6a5d04fe9
denz1994 <denz1994@gmail.com>
2019-12-16 12:16:49 -0500

    Refactor soundPlayer -> buttonSoundPlayer in carousel button options. https://github.com/phetsims/sun/issues/547

--- js/Carousel.js
+++ js/Carousel.js
@@ -58,7 +58,7 @@ define( require => {
     arrowStroke: 'black', // {Color|string} color used for the arrow icons
     arrowLineWidth: 3, // {number} line width used to stroke the arrow icons
     hideDisabledButtons: false, // {boolean} whether to hide buttons when they are disabled
-    soundPlayer: null,
+    buttonSoundPlayer: null,
 
     // for dilating pointer areas of next/previous buttons such that they do not overlap with Carousel content
     buttonTouchAreaXDilation: 0, // {number} horizontal touchArea dilation
