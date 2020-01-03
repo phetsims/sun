41b9006b93ad13754ccdbe2d31157a68b6ed9b53
phet-dev <phetadmin@gmail.com>
2020-01-03 03:28:09 -0700

    update copyright dates from daily grunt work

--- js/OnOffSwitch.js
+++ js/OnOffSwitch.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2019, University of Colorado Boulder
+// Copyright 2014-2020, University of Colorado Boulder
 
 /**
  * On/off switch, similar to iOS' UISwitch, used in iOS `'Settings' app.
