94ba703de67d16cfa4a7b3a14795e16cd53d8ab1
zepumph <michael.kauzmann@colorado.edu>
2020-01-02 17:47:38 -0900

    remove todo, https://github.com/phetsims/sun/issues/552

--- js/OnOffSwitch.js
+++ js/OnOffSwitch.js
@@ -164,8 +164,6 @@ define( require => {
 
         if ( options.toggleWhileDragging === true || ( isDraggedOutside && options.toggleWhileDragging === null ) ) {
 
-          // TODO https://github.com/phetsims/sun/issues/552 A way to distinguish between drag-to-toggle vs click-to-toggle
-
           // Only signify a change if the value actually changed to avoid duplicate messages in the PhET-iO Event
           // stream, see https://github.com/phetsims/phet-io/issues/369
           const changed = onProperty.get() !== value;
