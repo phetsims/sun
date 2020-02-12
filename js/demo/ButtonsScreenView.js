0d313e0ece0e78a66445d171ceec25606be35535
phet-dev <phetadmin@gmail.com>
2020-02-12 03:38:01 -0700

    update copyright dates from daily grunt work

--- js/demo/ButtonsScreenView.js
+++ js/demo/ButtonsScreenView.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2019, University of Colorado Boulder
+// Copyright 2014-2020, University of Colorado Boulder
 
 /**
  * Main ScreenView container for demonstrating and testing the various buttons.
