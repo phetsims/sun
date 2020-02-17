32b0a1c243eeb14a3fcb4cbe7bf43679119a211d
pixelzoom <cmalley@pixelzoom.com>
2020-02-17 13:42:47 -0700

    convert OnOffSwitch to ES6 class, https://github.com/phetsims/sun/issues/559

--- js/OnOffSwitch.js
+++ js/OnOffSwitch.js
@@ -20,7 +20,6 @@ define( require => {
   // modules
   const Dimension2 = require( 'DOT/Dimension2' );
   const EventType = require( 'TANDEM/EventType' );
-  const inherit = require( 'PHET_CORE/inherit' );
   const merge = require( 'PHET_CORE/merge' );
   const Node = require( 'SCENERY/nodes/Node' );
   const OnOffSwitchIO = require( 'SUN/OnOffSwitchIO' );
@@ -32,193 +31,207 @@ define( require => {
   const Utils = require( 'DOT/Utils' );
   const Vector2 = require( 'DOT/Vector2' );
 
-  /**
-   * @param {Property.<boolean>} onProperty
-   * @param {Object} [options]
-   * @constructor
-   */
-  function OnOffSwitch( onProperty, options ) {
+  // constants
+  const DEFAULT_SIZE = new Dimension2( 60, 30 );
 
-    options = merge( {
+  class OnOffSwitch extends Node {
 
-      size: new Dimension2( 60, 30 ), // if you want the thumb to be a circle, use width that is 2x height
-      cursor: 'pointer',
+    /**
+     * @param {Property.<boolean>} onProperty
+     * @param {Object} [options]
+     */
+    constructor( onProperty, options ) {
 
-      // controls the behavior of when model value changes occur during dragging (if any)
-      // null (default: triggers model changes when thumb is dragged far enough to the side, similar to iOS)
-      // true: triggers model changes whenever the thumb crosses sides
-      // false: only trigger model changes until release
-      toggleWhileDragging: null,
+      options = merge( {
 
-      // number of view-space units the drag needs to cover to be considered a "drag" instead of a "click/tap"
-      dragThreshold: 3,
+        size: DEFAULT_SIZE, // if you want the thumb to be a circle, use width that is 2x height
+        cursor: 'pointer',
 
-      // number of thumb-widths outside the normal range past where the model value will change
-      toggleThreshold: 1,
+        // controls the behavior of when model value changes occur during dragging (if any)
+        // null (default: triggers model changes when thumb is dragged far enough to the side, similar to iOS)
+        // true: triggers model changes whenever the thumb crosses sides
+        // false: only trigger model changes until release
+        toggleWhileDragging: null,
 
-      // thumb
-      thumbFill: 'white',
-      thumbStroke: 'black',
-      thumbTouchAreaXDilation: 8,
-      thumbTouchAreaYDilation: 8,
-      thumbMouseAreaXDilation: 0,
-      thumbMouseAreaYDilation: 0,
+        // number of view-space units the drag needs to cover to be considered a "drag" instead of a "click/tap"
+        dragThreshold: 3,
 
-      // track
-      trackOffFill: 'white', // track fill when onProperty is false
-      trackOnFill: 'rgb(0,200,0)', // track fill when onProperty is true
-      trackStroke: 'black',
+        // number of thumb-widths outside the normal range past where the model value will change
+        toggleThreshold: 1,
 
-      // phet-io
-      tandem: Tandem.REQUIRED,
-      phetioType: OnOffSwitchIO,
-      phetioEventType: EventType.USER
-    }, options );
+        // thumb
+        thumbFill: 'white',
+        thumbStroke: 'black',
+        thumbTouchAreaXDilation: 8,
+        thumbTouchAreaYDilation: 8,
+        thumbMouseAreaXDilation: 0,
+        thumbMouseAreaYDilation: 0,
 
-    const self = this;
+        // track
+        trackOffFill: 'white', // track fill when onProperty is false
+        trackOnFill: 'rgb( 0, 200, 0 )', // track fill when onProperty is true
+        trackStroke: 'black',
 
-    Node.call( this );
+        // phet-io
+        tandem: Tandem.REQUIRED,
+        phetioType: OnOffSwitchIO,
+        phetioEventType: EventType.USER
+      }, options );
 
-    const cornerRadius = options.size.height / 2;
+      super();
 
-    // @private track that the thumb slides in
-    this.trackNode = new Rectangle( 0, 0, options.size.width, options.size.height, cornerRadius, cornerRadius, {
-      fill: options.trackOffFill,
-      stroke: options.trackStroke,
-      cachedPaints: [ options.trackOnFill, options.trackOffFill ]
-    } );
-    this.addChild( this.trackNode );
+      const cornerRadius = options.size.height / 2;
 
-    // @private thumb (aka knob)
-    this.thumbNode = new Rectangle( 0, 0, 0.5 * options.size.width, options.size.height, cornerRadius, cornerRadius, {
-      fill: options.thumbFill,
-      stroke: options.thumbStroke
-    } );
-    this.addChild( this.thumbNode );
+      // @private track that the thumb slides in
+      this.trackNode = new Rectangle( 0, 0, options.size.width, options.size.height, cornerRadius, cornerRadius, {
+        fill: options.trackOffFill,
+        stroke: options.trackStroke,
+        cachedPaints: [ options.trackOnFill, options.trackOffFill ]
+      } );
+      this.addChild( this.trackNode );
 
-    // thumb touchArea
-    if ( options.thumbTouchAreaXDilation || options.thumbTouchAreaYDilation ) {
-      this.thumbNode.touchArea = Shape.roundRect(
-        -options.thumbTouchAreaXDilation, -options.thumbTouchAreaYDilation,
-        ( 0.5 * options.size.width ) + ( 2 * options.thumbTouchAreaXDilation ),
-        options.size.height + ( 2 * options.thumbTouchAreaYDilation ), cornerRadius, cornerRadius );
-    }
+      // @private thumb (aka knob)
+      this.thumbNode = new Rectangle( 0, 0, 0.5 * options.size.width, options.size.height, cornerRadius, cornerRadius, {
+        fill: options.thumbFill,
+        stroke: options.thumbStroke
+      } );
+      this.addChild( this.thumbNode );
 
-    // thumb mouseArea
-    if ( options.thumbMouseAreaXDilation || options.thumbMouseAreaYDilation ) {
-      this.thumbNode.mouseArea = Shape.roundRect(
-        -options.thumbMouseAreaXDilation, -options.thumbMouseAreaYDilation,
-        ( 0.5 * options.size.width ) + ( 2 * options.thumbMouseAreaXDilation ),
-        options.size.height + ( 2 * options.thumbMouseAreaYDilation ), cornerRadius, cornerRadius );
-    }
-
-    // move thumb to on or off position
-    const updateThumb = function( on ) {
-      if ( on ) {
-        self.thumbNode.right = options.size.width;
-        self.trackNode.fill = options.trackOnFill;
+      // thumb touchArea
+      if ( options.thumbTouchAreaXDilation || options.thumbTouchAreaYDilation ) {
+        this.thumbNode.touchArea = Shape.roundRect(
+          -options.thumbTouchAreaXDilation, -options.thumbTouchAreaYDilation,
+          ( 0.5 * options.size.width ) + ( 2 * options.thumbTouchAreaXDilation ),
+          options.size.height + ( 2 * options.thumbTouchAreaYDilation ), cornerRadius, cornerRadius );
       }
-      else {
-        self.thumbNode.left = 0;
-        self.trackNode.fill = options.trackOffFill;
+
+      // thumb mouseArea
+      if ( options.thumbMouseAreaXDilation || options.thumbMouseAreaYDilation ) {
+        this.thumbNode.mouseArea = Shape.roundRect(
+          -options.thumbMouseAreaXDilation, -options.thumbMouseAreaYDilation,
+          ( 0.5 * options.size.width ) + ( 2 * options.thumbMouseAreaXDilation ),
+          options.size.height + ( 2 * options.thumbMouseAreaYDilation ), cornerRadius, cornerRadius );
       }
-    };
 
-    // sync with onProperty
-    onProperty.link( updateThumb.bind( this ) );
-
-    // thumb interactivity
-    const dragThresholdSquared = options.dragThreshold * options.dragThreshold; // comparing squared magnitudes is a bit faster
-    const accumulatedDelta = new Vector2( 0, 0 ); // stores how far we are from where our drag started, in our local coordinate frame
-    let passedDragThreshold = false; // whether we have dragged far enough to be considered for "drag" behavior (pick closest side), or "tap" behavior (toggle)
-
-    this.addInputListener( new SimpleDragHandler( {
-      tandem: options.tandem.createTandem( 'simpleDragHandler' ),
-
-      // only touch to snag when over the thumb (don't snag on the track itself)
-      allowTouchSnag: function( evt ) {
-        return _.includes( evt.trail.nodes, self.thumbNode );
-      },
-
-      start: function( evt, trail ) {
-        // resets our state
-        accumulatedDelta.setXY( 0, 0 ); // reset it mutably (less allocation)
-        passedDragThreshold = false;
-      },
-
-      drag: function( evt, trail ) {
-        // center the thumb on the pointer's x-coordinate if possible (but clamp to left and right ends)
-        const viewPoint = evt.currentTarget.globalToLocalPoint( evt.pointer.point );
-        const halfThumbWidth = self.thumbNode.width / 2;
-        self.thumbNode.centerX = Utils.clamp( viewPoint.x, halfThumbWidth, options.size.width - halfThumbWidth );
-
-        // whether the thumb is dragged outside of the possible range far enough beyond our threshold to potentially
-        // trigger an immediate model change
-        const isDraggedOutside = viewPoint.x < ( 1 - 2 * options.toggleThreshold ) * halfThumbWidth ||
-                                 viewPoint.x > ( -1 + 2 * options.toggleThreshold ) * halfThumbWidth + options.size.width;
-
-        const value = self.thumbPositionToValue(); // value represented by the current thumb position
-
-        // track fill changes based on the thumb positions
-        self.trackNode.fill = value ? options.trackOnFill : options.trackOffFill;
-
-        if ( options.toggleWhileDragging === true || ( isDraggedOutside && options.toggleWhileDragging === null ) ) {
-
-          // Only signify a change if the value actually changed to avoid duplicate messages in the PhET-iO Event
-          // stream, see https://github.com/phetsims/phet-io/issues/369
-          const changed = onProperty.get() !== value;
-          if ( changed ) {
-            self.phetioStartEvent( 'toggled', {
-              data: {
-                oldValue: !value,
-                newValue: value
-              }
-            } );
-            onProperty.set( value );
-            self.phetioEndEvent();
-          }
+      // move thumb to on or off position
+      const updateThumb = on => {
+        if ( on ) {
+          this.thumbNode.right = options.size.width;
+          this.trackNode.fill = options.trackOnFill;
         }
-      },
+        else {
+          this.thumbNode.left = 0;
+          this.trackNode.fill = options.trackOffFill;
+        }
+      };
 
-      end: function( evt, trail ) {
-        const oldValue = onProperty.get();
+      // sync with onProperty, must be unlinked in dispose
+      onProperty.link( updateThumb );
 
-        // if moved past the threshold, choose value based on the side, otherwise just toggle
-        const newValue = passedDragThreshold ? self.thumbPositionToValue() : !onProperty.get();
+      // thumb interactivity
+      const dragThresholdSquared = options.dragThreshold * options.dragThreshold; // comparing squared magnitudes is a bit faster
+      const accumulatedDelta = new Vector2( 0, 0 ); // stores how far we are from where our drag started, in our local coordinate frame
+      let passedDragThreshold = false; // whether we have dragged far enough to be considered for "drag" behavior (pick closest side), or "tap" behavior (toggle)
 
-        self.phetioStartEvent( 'toggled', {
-          data: {
-            oldValue: oldValue,
-            newValue: newValue
+      this.addInputListener( new SimpleDragHandler( {
+        tandem: options.tandem.createTandem( 'simpleDragHandler' ),
+
+        // only touch to snag when over the thumb (don't snag on the track itself)
+        allowTouchSnag: event => _.includes( event.trail.nodes, this.thumbNode ),
+
+        start: ( event, trail ) => {
+          // resets our state
+          accumulatedDelta.setXY( 0, 0 ); // reset it mutably (less allocation)
+          passedDragThreshold = false;
+        },
+
+        drag: ( event, trail ) => {
+          // center the thumb on the pointer's x-coordinate if possible (but clamp to left and right ends)
+          const viewPoint = event.currentTarget.globalToLocalPoint( event.pointer.point );
+          const halfThumbWidth = this.thumbNode.width / 2;
+          this.thumbNode.centerX = Utils.clamp( viewPoint.x, halfThumbWidth, options.size.width - halfThumbWidth );
+
+          // whether the thumb is dragged outside of the possible range far enough beyond our threshold to potentially
+          // trigger an immediate model change
+          const isDraggedOutside = viewPoint.x < ( 1 - 2 * options.toggleThreshold ) * halfThumbWidth ||
+                                   viewPoint.x > ( -1 + 2 * options.toggleThreshold ) * halfThumbWidth + options.size.width;
+
+          const value = this.thumbPositionToValue(); // value represented by the current thumb position
+
+          // track fill changes based on the thumb positions
+          this.trackNode.fill = value ? options.trackOnFill : options.trackOffFill;
+
+          if ( options.toggleWhileDragging === true || ( isDraggedOutside && options.toggleWhileDragging === null ) ) {
+
+            // Only signify a change if the value actually changed to avoid duplicate messages in the PhET-iO Event
+            // stream, see https://github.com/phetsims/phet-io/issues/369
+            const changed = onProperty.get() !== value;
+            if ( changed ) {
+              this.phetioStartEvent( 'toggled', {
+                data: {
+                  oldValue: !value,
+                  newValue: value
+                }
+              } );
+              onProperty.set( value );
+              this.phetioEndEvent();
+            }
           }
-        } );
+        },
 
-        onProperty.set( newValue );
+        end: ( event, trail ) => {
+          const oldValue = onProperty.get();
 
-        // update the thumb location (sanity check that it's here, only needs to be run if passedDragThreshold===true)
-        updateThumb( onProperty.get() );
+          // if moved past the threshold, choose value based on the side, otherwise just toggle
+          const newValue = passedDragThreshold ? this.thumbPositionToValue() : !onProperty.get();
 
-        self.phetioEndEvent();
-      },
+          this.phetioStartEvent( 'toggled', {
+            data: {
+              oldValue: oldValue,
+              newValue: newValue
+            }
+          } );
 
-      translate: function( params ) {
-        accumulatedDelta.add( params.delta );
-        passedDragThreshold = passedDragThreshold || ( accumulatedDelta.magnitudeSquared > dragThresholdSquared );
-      }
-    } ) );
+          onProperty.set( newValue );
 
-    this.mutate( options );
-  }
+          // update the thumb location (sanity check that it's here, only needs to be run if passedDragThreshold===true)
+          updateThumb( onProperty.get() );
 
-  sun.register( 'OnOffSwitch', OnOffSwitch );
+          this.phetioEndEvent();
+        },
 
-  return inherit( Node, OnOffSwitch, {
+        translate: params => {
+          accumulatedDelta.add( params.delta );
+          passedDragThreshold = passedDragThreshold || ( accumulatedDelta.magnitudeSquared > dragThresholdSquared );
+        }
+      } ) );
+
+      this.mutate( options );
+
+      // @private
+      this.disposeOnOffSwitch = () => {
+        onProperty.unlink( updateThumb );
+      };
+    }
+
+    /**
+     * @public
+     * @override
+     */
+    dispose() {
+      this.disposeOnOffSwitch();
+      super.dispose();
+    }
+
     /*
      * Converts the thumb position to a boolean on/off value.
      * Off (false) is to the left, on (true) is to the right.
+     * @private
      */
-    thumbPositionToValue: function() {
+    thumbPositionToValue() {
       return this.thumbNode.centerX > this.trackNode.centerX;
     }
-  } );
+  }
+
+  return sun.register( 'OnOffSwitch', OnOffSwitch );
 } );
