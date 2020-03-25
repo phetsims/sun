24a53bf50989529d3c6c8ebd94a8741142967225
Jesse <jesse.greenberg@colorado.edu>
2020-03-25 11:49:23 -0400

    Back to 'pressed' instead of 'checked' for screen reader output, see phetsims/molecules-and-light#296

--- js/buttons/RoundStickyToggleButton.js
+++ js/buttons/RoundStickyToggleButton.js
@@ -45,7 +45,7 @@ function RoundStickyToggleButton( valueUp, valueDown, property, options ) {
   toggleButtonModel.produceSoundEmitter.addListener( playSound );
 
   // PDOM - signify button is 'pressed' when down
-  const setAriaPressed = value => this.setAccessibleAttribute( 'aria-checked', property.value === valueDown );
+  const setAriaPressed = value => this.setAccessibleAttribute( 'aria-pressed', property.value === valueDown );
   property.link( setAriaPressed );
 
   // @private - dispose items specific to this instance
