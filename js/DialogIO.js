5dcd04e209f5849900025be28ccea096fa218661
zepumph <michael.kauzmann@colorado.edu>
2020-05-20 14:36:07 -0800

    add AboutDialog to state, https://github.com/phetsims/phet-io/issues/1663

--- js/DialogIO.js
+++ js/DialogIO.js
@@ -11,7 +11,21 @@ import NodeIO from '../../scenery/js/nodes/NodeIO.js';
 import ObjectIO from '../../tandem/js/types/ObjectIO.js';
 import sun from './sun.js';
 
-class DialogIO extends NodeIO {}
+class DialogIO extends NodeIO {
+
+  /**
+   * Since many Dialogs are dynamic elements, these need to be in the state. The value of the state object doesn't
+   * matter, but it instead just serves as a marker to tell the state engine to recreate the Dialog (if dynamic) when
+   * setting state.
+   * @override
+   * @public
+   * @param {Dialog} dialog
+   * @returns {string}
+   */
+  static toStateObject( dialog ) {
+    return dialog.tandem.phetioID;
+  }
+}
 
 DialogIO.documentation = 'A dialog panel';
 DialogIO.validator = { isValidValue: v => v instanceof phet.sun.Dialog };
