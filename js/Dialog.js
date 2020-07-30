9b6088415131300a71482eda0cbdb1cac3f2f5ce
zepumph <michael.kauzmann@colorado.edu>
2020-07-30 08:07:57 -0800

    dispose isShowingProperty, https://github.com/phetsims/phet-io-wrappers/issues/360 https://github.com/phetsims/joist/issues/586

--- js/Dialog.js
+++ js/Dialog.js
@@ -368,6 +368,8 @@ function Dialog( content, options ) {
     this.updateLayoutMultilink.dispose();
     this.removeInputListener( escapeListener );
 
+    this.isShowingProperty.dispose();
+
     closeButton.dispose();
 
     // remove dialog content from scene graph, but don't dispose because Panel
