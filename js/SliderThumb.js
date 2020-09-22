6351a940918f1f2de223731b8b966e9d6db6395f
pixelzoom <cmalley@pixelzoom.com>
2020-09-22 16:02:04 -0600

    convert SliderThumb to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/SliderThumb.js
+++ js/SliderThumb.js
@@ -9,7 +9,6 @@
 
 import Dimension2 from '../../dot/js/Dimension2.js';
 import Shape from '../../kite/js/Shape.js';
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import PressListener from '../../scenery/js/listeners/PressListener.js';
 import Path from '../../scenery/js/nodes/Path.js';
@@ -17,64 +16,60 @@ import Rectangle from '../../scenery/js/nodes/Rectangle.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import sun from './sun.js';
 
-/**
- * @param {Object} [options] see HSlider constructor
- * @constructor
- * @private
- */
-function SliderThumb( options ) {
+class SliderThumb extends Rectangle {
+  /**
+   * @param {Object} [options] see HSlider constructor
+   */
+  constructor( options ) {
 
-  options = merge( {
-    size: new Dimension2( 22, 45 ),
-    fill: 'rgb(50,145,184)',
-    fillHighlighted: 'rgb(71,207,255)',
-    stroke: 'black',
-    lineWidth: 1,
-    centerLineStroke: 'white',
-    tandem: Tandem.REQUIRED
-  }, options );
+    options = merge( {
+      size: new Dimension2( 22, 45 ),
+      fill: 'rgb(50,145,184)',
+      fillHighlighted: 'rgb(71,207,255)',
+      stroke: 'black',
+      lineWidth: 1,
+      centerLineStroke: 'white',
+      tandem: Tandem.REQUIRED
+    }, options );
 
-  // rectangle
-  const arcWidth = 0.25 * options.size.width;
-  Rectangle.call( this, 0, 0,
-    options.size.width, options.size.height,
-    arcWidth, arcWidth, merge( options, {
-      fill: options.fill,
-      stroke: options.stroke,
-      lineWidth: options.lineWidth,
-      cachedPaints: [ options.fill, options.fillHighlighted ]
-    } )
-  );
+    // Set a default corner radius
+    if ( options.cornerRadius === undefined ) {
+      options.cornerRadius = 0.25 * options.size.width;
+    }
 
-  // Paint area that is slightly larger than the slider thumb so SVG updates a large enough paintable region.
-  // Related to https://github.com/phetsims/masses-and-springs/issues/334
-  const paintLayer = Rectangle.bounds( this.bounds.dilated( 5 ), {
-    fill: 'transparent',
-    localBounds: this.bounds,
-    pickable: false
-  } );
-  this.addChild( paintLayer );
+    assert && assert( options.cachedPaints === undefined, 'SliderThumb sets cachedPaints' );
+    options.cachedPaints = [ options.fill, options.fillHighlighted ];
 
-  // vertical line down the center
-  const centerLineYMargin = 3;
-  this.addChild( new Path( Shape.lineSegment(
-    options.size.width / 2, centerLineYMargin,
-    options.size.width / 2, options.size.height - centerLineYMargin ), {
-    stroke: options.centerLineStroke
-  } ) );
+    super( 0, 0, options.size.width, options.size.height, options );
 
-  // highlight thumb on pointer over
-  const pressListener = new PressListener( {
-    attach: false,
-    tandem: Tandem.OPT_OUT
-  } );
-  pressListener.isHighlightedProperty.link( isHighlighted => {
-    this.fill = isHighlighted ? options.fillHighlighted : options.fill;
-  } );
-  this.addInputListener( pressListener );
+    // Paint area that is slightly larger than the slider thumb so SVG updates a large enough paintable region.
+    // Related to https://github.com/phetsims/masses-and-springs/issues/334
+    const paintLayer = Rectangle.bounds( this.bounds.dilated( 5 ), {
+      fill: 'transparent',
+      localBounds: this.bounds,
+      pickable: false
+    } );
+    this.addChild( paintLayer );
+
+    // vertical line down the center
+    const centerLineYMargin = 3;
+    this.addChild( new Path( Shape.lineSegment(
+      options.size.width / 2, centerLineYMargin,
+      options.size.width / 2, options.size.height - centerLineYMargin ), {
+      stroke: options.centerLineStroke
+    } ) );
+
+    // highlight thumb on pointer over
+    const pressListener = new PressListener( {
+      attach: false,
+      tandem: Tandem.OPT_OUT
+    } );
+    pressListener.isHighlightedProperty.link( isHighlighted => {
+      this.fill = isHighlighted ? options.fillHighlighted : options.fill;
+    } );
+    this.addInputListener( pressListener );
+  }
 }
 
 sun.register( 'SliderThumb', SliderThumb );
-
-inherit( Rectangle, SliderThumb );
 export default SliderThumb;
