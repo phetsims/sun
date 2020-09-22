c2a4b67c868992e3d462adf6f4d2510782c6c479
pixelzoom <cmalley@pixelzoom.com>
2020-09-22 17:54:44 -0600

    convert BooleanRoundStickyToggleButton to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/buttons/BooleanRoundStickyToggleButton.js
+++ js/buttons/BooleanRoundStickyToggleButton.js
@@ -1,29 +1,25 @@
 // Copyright 2014-2020, University of Colorado Boulder
 
 /**
- * A circular toggle button that switches the value of a boolean property.  It sticks in the down position when pressed,
- * popping back up when pressed again.
- *
- * This class inherits from the more general RoundStickyToggleButton, which can take any values.
+ * A round toggle button that toggles the value of a boolean Property.
  *
  * @author John Blanco (PhET Interactive Simulations)
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-import inherit from '../../../phet-core/js/inherit.js';
 import sun from '../sun.js';
 import RoundStickyToggleButton from './RoundStickyToggleButton.js';
 
-/**
- * @param {Property.<boolean>} booleanProperty
- * @param {Object} [options]
- * @constructor
- */
-function BooleanRoundStickyToggleButton( booleanProperty, options ) {
-  RoundStickyToggleButton.call( this, false, true, booleanProperty, options );
+class BooleanRoundStickyToggleButton extends RoundStickyToggleButton {
+
+  /**
+   * @param {Property.<boolean>} booleanProperty
+   * @param {Object} [options]
+   */
+  constructor( booleanProperty, options ) {
+    super( false, true, booleanProperty, options );
+  }
 }
 
 sun.register( 'BooleanRoundStickyToggleButton', BooleanRoundStickyToggleButton );
-
-inherit( RoundStickyToggleButton, BooleanRoundStickyToggleButton );
 export default BooleanRoundStickyToggleButton;
