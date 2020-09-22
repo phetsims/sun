6fdf275a1729dc63221001b544c6e56a3e567cf1
pixelzoom <cmalley@pixelzoom.com>
2020-09-22 17:15:55 -0600

    convert ColorConstants to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/ColorConstants.js
+++ js/ColorConstants.js
@@ -6,26 +6,15 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-import inherit from '../../phet-core/js/inherit.js';
 import Color from '../../scenery/js/util/Color.js';
-import sun from './sun.js';
 
-/**
- *
- * @constructor
- */
-function ColorConstants() {
-}
-
-sun.register( 'ColorConstants', ColorConstants );
-
-inherit( Object, ColorConstants, {}, {
+const ColorConstants = {
 
   //The default blue color used in many places, for buttons
   LIGHT_BLUE: new Color( 153, 206, 255 ),
 
   //Light gray, used as the 'disabled' color in many places.
   LIGHT_GRAY: new Color( 220, 220, 220 )
-} );
+};
 
 export default ColorConstants;
