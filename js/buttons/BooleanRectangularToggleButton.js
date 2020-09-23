a8f322081ac7ba64e7dbffb79d2421b2bbaaed1a
pixelzoom <cmalley@pixelzoom.com>
2020-09-22 18:03:26 -0600

    convert BooleanRectangularToggleButton and subclasses to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/buttons/BooleanRectangularToggleButton.js
+++ js/buttons/BooleanRectangularToggleButton.js
@@ -4,45 +4,46 @@
  * This toggle button uses a boolean property and a trueNode and falseNode to display its content.
  */
 
-import inherit from '../../../phet-core/js/inherit.js';
 import merge from '../../../phet-core/js/merge.js';
 import Tandem from '../../../tandem/js/Tandem.js';
 import BooleanToggleNode from '../BooleanToggleNode.js';
 import sun from '../sun.js';
 import RectangularToggleButton from './RectangularToggleButton.js';
 
-/**
- * @param {Node} trueNode
- * @param {Node} falseNode
- * @param {Property.<boolean>} booleanProperty
- * @param {Object} [options]
- * @constructor
- */
-function BooleanRectangularToggleButton( trueNode, falseNode, booleanProperty, options ) {
+class BooleanRectangularToggleButton extends RectangularToggleButton {
 
-  options = merge( { tandem: Tandem.REQUIRED }, options );
+  /**
+   * @param {Node} trueNode
+   * @param {Node} falseNode
+   * @param {Property.<boolean>} booleanProperty
+   * @param {Object} [options]
+   */
+  constructor( trueNode, falseNode, booleanProperty, options ) {
 
-  assert && assert( !options.content, 'options.content cannot be set' );
-  options.content = new BooleanToggleNode( trueNode, falseNode, booleanProperty );
+    options = merge( {
+      tandem: Tandem.REQUIRED
+    }, options );
 
-  RectangularToggleButton.call( this, false, true, booleanProperty, options );
+    assert && assert( !options.content, 'options.content cannot be set' );
+    options.content = new BooleanToggleNode( trueNode, falseNode, booleanProperty );
 
-  // @private {function} - internally used disposal function
-  this.disposeBooleanRectangularToggleButton = function() {
-    options.content.dispose();
-  };
+    super( false, true, booleanProperty, options );
+
+    // @private
+    this.disposeBooleanRectangularToggleButton = () => {
+      options.content && options.content.dispose();
+    };
+  }
+
+  /**
+   * @public
+   * @override
+   */
+  dispose() {
+    this.disposeBooleanRectangularToggleButton();
+    super.dispose();
+  }
 }
 
 sun.register( 'BooleanRectangularToggleButton', BooleanRectangularToggleButton );
-
-inherit( RectangularToggleButton, BooleanRectangularToggleButton, {
-  /**
-   * @public
-   */
-  dispose: function() {
-    this.disposeBooleanRectangularToggleButton();
-    RectangularToggleButton.prototype.dispose.call( this );
-  }
-} );
-
 export default BooleanRectangularToggleButton;
