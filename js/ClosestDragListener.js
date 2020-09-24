fa3c1cd8339ba0f49eb1eae05bfa69b5c37b8086
Jonathan Olson <jonathan.olson@colorado.edu>
2020-09-23 21:03:11 -0600

    Documentation update for ClosestDragListener, see https://github.com/phetsims/sun/issues/629

--- js/ClosestDragListener.js
+++ js/ClosestDragListener.js
@@ -1,8 +1,9 @@
 // Copyright 2016-2020, University of Colorado Boulder
 
 /**
- * A Scenery input listener that is able to find the closest in a list of nodes to a "down" event and trigger a drag
- * on the closest (assuming it has a SimpleDragHandler).
+ * A Scenery input listener that is able to find the closest in a list of items to a "down" event and trigger an action
+ * (usually a drag) on that item. Usually this will be a drag listener start/press (e.g. SimpleDragHandler/DragListener),
+ * but could accommodate other uses. It's similar in use to DragListener.createForwardingListener.
  *
  * Handles items of the form:
  * {
