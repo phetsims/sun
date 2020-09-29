59932b7d6b616846584d8690702fbf3260df8d36
pixelzoom <cmalley@pixelzoom.com>
2020-09-28 23:55:50 -0600

    convert MomentaryButtonModel to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/buttons/MomentaryButtonModel.js
+++ js/buttons/MomentaryButtonModel.js
@@ -6,67 +6,70 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import inherit from '../../../phet-core/js/inherit.js';
 import merge from '../../../phet-core/js/merge.js';
 import PhetioObject from '../../../tandem/js/PhetioObject.js';
 import Tandem from '../../../tandem/js/Tandem.js';
 import sun from '../sun.js';
 import ButtonModel from './ButtonModel.js';
 
-/**
- * @param {Object} valueOff - value when the button is in the off state
- * @param {Object} valueOn - value when the button is in the on state
- * @param {Property} valueProperty
- * @param {Object} [options]
- * @constructor
- */
-function MomentaryButtonModel( valueOff, valueOn, valueProperty, options ) {
-  const self = this;
+class MomentaryButtonModel extends ButtonModel {
 
-  // Tandem support
-  options = merge( {
-    tandem: Tandem.REQUIRED,
-    phetioReadOnly: PhetioObject.DEFAULT_OPTIONS.phetioReadOnly // to support properly passing this to children, see https://github.com/phetsims/tandem/issues/60
-  }, options );
+  /**
+   * @param {Object} valueOff - value when the button is in the off state
+   * @param {Object} valueOn - value when the button is in the on state
+   * @param {Property} valueProperty
+   * @param {Object} [options]
+   */
+  constructor( valueOff, valueOn, valueProperty, options ) {
 
-  ButtonModel.call( self, options );
+    // Tandem support
+    options = merge( {
 
-  const downListener = function( down ) {
+      // phet-io
+      tandem: Tandem.REQUIRED,
 
-    // turn on when pressed (if enabled)
-    if ( down ) {
-      if ( self.enabledProperty.get() ) {
-        valueProperty.set( valueOn );
+      // to support properly passing this to children, see https://github.com/phetsims/tandem/issues/60
+      phetioReadOnly: PhetioObject.DEFAULT_OPTIONS.phetioReadOnly
+    }, options );
+
+    super( options );
+
+    const downListener = down => {
+
+      // turn on when pressed (if enabled)
+      if ( down ) {
+        if ( this.enabledProperty.get() ) {
+          valueProperty.set( valueOn );
+        }
       }
-    }
-    else {
-      valueProperty.set( valueOff );
-    }
-  };
-  this.downProperty.lazyLink( downListener );
+      else {
+        valueProperty.set( valueOff );
+      }
+    };
+    this.downProperty.lazyLink( downListener );
 
-  // if valueProperty set externally, signify to ButtonModel
-  const valuePropertyListener = function( value ) {
-    self.downProperty.set( value === valueOn );
-  };
-  valueProperty.link( valuePropertyListener );
+    // if valueProperty set externally, signify to ButtonModel
+    const valuePropertyListener = value => {
+      this.downProperty.set( value === valueOn );
+    };
+    valueProperty.link( valuePropertyListener );
 
-  // @private: just for dispose.  Named based on the type name so it won't have a name collision with parent/child ones
-  this.disposeMomentaryButtonModel = function() {
-    self.downProperty.unlink( downListener );
-    valueProperty.unlink( valuePropertyListener );
-  };
+    // @private
+    this.disposeMomentaryButtonModel = () => {
+      this.downProperty.unlink( downListener );
+      valueProperty.unlink( valuePropertyListener );
+    };
+  }
+
+  /**
+   * @public
+   * @override
+   */
+  dispose() {
+    this.disposeMomentaryButtonModel();
+    super.dispose(); //TODO fails with assertions enabled, see sun#212
+  }
 }
 
 sun.register( 'MomentaryButtonModel', MomentaryButtonModel );
-
-inherit( ButtonModel, MomentaryButtonModel, {
-
-  // @public - Ensures that this model is eligible for GC.
-  dispose: function() {
-    this.disposeMomentaryButtonModel();
-    ButtonModel.prototype.dispose.call( this ); //TODO fails with assertions enabled, see sun#212
-  }
-} );
-
 export default MomentaryButtonModel;
