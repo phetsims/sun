954bb3672babfdcef084b60c2a08df57573553d6
samreid <reids@colorado.edu>
2020-09-30 09:34:55 -0600

    Move IO Type to core file, see https://github.com/phetsims/tandem/issues/212

--- js/buttons/RoundMomentaryButton.js
+++ js/buttons/RoundMomentaryButton.js
@@ -8,12 +8,13 @@
  */
 
 import merge from '../../../phet-core/js/merge.js';
+import Node from '../../../scenery/js/nodes/Node.js';
 import Tandem from '../../../tandem/js/Tandem.js';
+import IOType from '../../../tandem/js/types/IOType.js';
 import sun from '../sun.js';
 import MomentaryButtonInteractionStateProperty from './MomentaryButtonInteractionStateProperty.js';
 import MomentaryButtonModel from './MomentaryButtonModel.js';
 import RoundButtonView from './RoundButtonView.js';
-import RoundMomentaryButtonIO from './RoundMomentaryButtonIO.js';
 
 class RoundMomentaryButton extends RoundButtonView {
 
@@ -27,7 +28,7 @@ class RoundMomentaryButton extends RoundButtonView {
 
     options = merge( {
       tandem: Tandem.REQUIRED,
-      phetioType: RoundMomentaryButtonIO
+      phetioType: RoundMomentaryButton.RoundMomentaryButtonIO
     }, options );
 
     // Note it shares a tandem with this, so the emitter will be instrumented as a child of the button
@@ -48,6 +49,13 @@ class RoundMomentaryButton extends RoundButtonView {
     super.dispose();
   }
 }
+
+RoundMomentaryButton.RoundMomentaryButtonIO = new IOType( 'RoundMomentaryButtonIO', {
+  valueType: RoundMomentaryButton,
+  supertype: Node.NodeIO,
+  documentation: 'Button that performs an action while it is being pressed, and stops the action when released',
+  events: [ 'pressed', 'released', 'releasedDisabled' ]
+} );
 
 sun.register( 'RoundMomentaryButton', RoundMomentaryButton );
 export default RoundMomentaryButton;
