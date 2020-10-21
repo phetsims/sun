aca17cdd791dac6fddcd098f8089505b73b61711
zepumph <michael.kauzmann@colorado.edu>
2020-10-21 14:28:35 -0800

    formatting, https://github.com/phetsims/tandem/issues/222

--- js/Dialog.js
+++ js/Dialog.js
@@ -149,7 +149,7 @@ class Dialog extends Popupable( Panel ) {
     }, options );
 
     // Wait until merge complete to determine this value
-    options.visiblePropertyOptions = {phetioState: options.phetioState};
+    options.visiblePropertyOptions = { phetioState: options.phetioState };
 
     assert && assert( options.xMargin === undefined, 'Dialog sets xMargin' );
     options.xMargin = 0;
