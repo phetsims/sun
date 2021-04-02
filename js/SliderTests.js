2d9d1fe549192eb357ea9da9eb65f5909300a0cb
zepumph <michael.kauzmann@colorado.edu>
2021-04-02 12:14:47 -0800

    remove phetioAPITest and all *API.js files, general cleanup, https://github.com/phetsims/phet-io/issues/1657

--- js/SliderTests.js
+++ js/SliderTests.js
@@ -12,11 +12,8 @@ import BooleanProperty from '../../axon/js/BooleanProperty.js';
 import Property from '../../axon/js/Property.js';
 import TinyProperty from '../../axon/js/TinyProperty.js';
 import Range from '../../dot/js/Range.js';
-import phetioAPITest from '../../tandem/js/phetioAPITest.js';
 import Tandem from '../../tandem/js/Tandem.js';
-import NumberIO from '../../tandem/js/types/NumberIO.js';
 import HSlider from './HSlider.js';
-import SliderAPI from './SliderAPI.js';
 
 QUnit.module( 'Slider' );
 
@@ -53,38 +50,3 @@ function testEnabledNode( assert, enabledNode, message ) {
   assert.ok( enabledNode.enabledProperty.value === enabledNode.enabled, `${message}: test getter after setting` );
   assert.ok( enabledNode.enabledProperty.value === false, `${message}: test getter after setting` );
 }
-
-QUnit.test( 'Slider PhET-iO API validation', assert => {
-  phetioAPITest( assert, new SliderAPI(), 'slider',
-    ( tandem, disposeEmitter ) => {
-      const valueProperty = new Property( 0, {
-        phetioType: Property.PropertyIO( NumberIO ),
-        tandem: tandem.createTandem( 'myValueProperty' )
-      } );
-      disposeEmitter.addListener( () => valueProperty.dispose() );
-      return new HSlider( valueProperty, new Range( 0, 10 ), { tandem: tandem } );
-    } );
-} );
-
-QUnit.test( 'Slider PhET-iO API validation, provided enabledProperty', assert => {
-  phetioAPITest( assert, new SliderAPI(), 'slider',
-
-    ( tandem, disposeEmitter ) => {
-      const valueProperty = new Property( 0, {
-        phetioType: Property.PropertyIO( NumberIO ),
-        tandem: tandem.createTandem( 'myValueProperty' )
-      } );
-      disposeEmitter.addListener( () => valueProperty.dispose() );
-
-      const enabledProperty = new BooleanProperty( false, {
-        tandem: tandem.createTandem( 'otherEnabled2Property' ),
-        phetioFeatured: true
-      } );
-      disposeEmitter.addListener( () => enabledProperty.dispose() );
-
-      return new HSlider( valueProperty, new Range( 0, 10 ), {
-        tandem: tandem,
-        enabledProperty: enabledProperty
-      } );
-    } );
-} );
