7f298004734daeeb0870aa53636618ad14cb84a3
zepumph <michael.kauzmann@colorado.edu>
2021-05-06 17:23:36 -0800

    update common code PDOM type doc, https://github.com/phetsims/tasks/issues/1080

--- js/accessibility/AccessibleValueHandler.js
+++ js/accessibility/AccessibleValueHandler.js
@@ -2,18 +2,18 @@
 
 /**
  * A trait for subtypes of Node. Meant for Nodes with a value that "run" on a NumberProperty and handles formatting,
- * mapping, and aria-valuetext updating.
+ * mapping, and aria-valuetext updating in the PDOM.
  *
  * Also implements the listeners that respond to accessible input, such as keydown, keyup, input, and change
- * events, which may come from a keyboard or other assistive device. Bind and add these as input listeners to the
- * node mixing in this trait.
+ * events, which may come from a keyboard or other assistive device. Use getAccessibleValueHandlerInputListener() to get
+ * these listeners to add to your Node with addInputListener().
  *
  * Browsers have limitations for the interaction of a slider when the range is not evenly divisible by the step size.
  * Rather than allow the browser to natively change the valueProperty with an input event, this trait implements a
  * totally custom interaction keeping the general slider behavior the same.
  *
  * @author Michael Kauzmann (PhET Interactive Simulations)
- * @author Jesse Greenberg
+ * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
 import Property from '../../../axon/js/Property.js';
