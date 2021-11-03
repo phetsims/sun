c0a9a0968f126f141194b65928a3b9e5e1747c7d
phet-dev <phetadmin@gmail.com>
2021-11-03 03:20:57 -0600

    update copyright dates from daily grunt work

--- js/DefaultSliderTrack.js
+++ js/DefaultSliderTrack.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2021, University of Colorado Boulder
 
 /**
  * DefaultSliderTrack is composed of two rectangles, one for the enabled section of the track and one for the disabled
