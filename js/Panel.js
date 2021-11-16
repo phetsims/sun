da619e1be17d20cd64cafdf8af36c4ac5c72cb4b
Jonathan Olson <jonathan.olson@colorado.edu>
2021-11-16 12:03:50 -0700

    Panel switch to use LayoutConstraint internally instead of custom listeners and handling, see https://github.com/phetsims/sun/issues/48

--- js/Panel.js
+++ js/Panel.js
@@ -11,6 +11,7 @@
 
 import merge from '../../phet-core/js/merge.js';
 import HeightSizable from '../../scenery/js/layout/HeightSizable.js';
+import LayoutConstraint from '../../scenery/js/layout/LayoutConstraint.js';
 import WidthSizable from '../../scenery/js/layout/WidthSizable.js';
 import Node from '../../scenery/js/nodes/Node.js';
 import Rectangle from '../../scenery/js/nodes/Rectangle.js';
@@ -56,9 +57,11 @@ class Panel extends WidthSizable( HeightSizable( Node ) ) {
 
     super();
 
-    const backgroundContainer = new Node();
+    // @private {Node}
+    this._content = content;
+    this._backgroundContainer = new Node();
 
-    // @private {Rectangle} - correct size will be set by updateBackground
+    // @private {Rectangle} - correct size will be set by layout
     this.background = new Rectangle( 0, 0, 1, 1, {
       lineWidth: options.lineWidth,
       pickable: options.backgroundPickable,
@@ -66,150 +69,205 @@ class Panel extends WidthSizable( HeightSizable( Node ) ) {
       cornerRadius: options.cornerRadius
     } );
 
-    // update the fill and stroke
+    // update the fill and stroke (before layout)
     this.setStroke( options.stroke );
     this.setFill( options.fill );
 
-    this.addChild( backgroundContainer );
+    this.addChild( this._backgroundContainer );
     this.addChild( content );
 
-    // flag for preventing recursion
-    let backgroundUpdateInProgress = false;
+    // @private {PanelConstraint}
+    this._constraint = new PanelConstraint( this, options );
+    this._constraint.updateLayout();
 
-    // Adjust the background size to match the content.
-    const updateBackground = () => {
-
-      // Check that an update isn't already in progress, lest we end up with some nasty recursion.  For details, please
-      // see https://github.com/phetsims/sun/issues/110 and https://github.com/phetsims/molecule-shapes/issues/130.
-      if ( backgroundUpdateInProgress ) {
-        return;
-      }
-
-      const hasValidContent = this.isChildIncludedInLayout( content );
-      backgroundContainer.children = hasValidContent ? [ this.background ] : [];
-      if ( !hasValidContent ) {
-        // Bail out (and make the background invisible) if our bounds are invalid
-        this.minimumWidth = null;
-        this.minimumHeight = null;
-        return;
-      }
-
-      backgroundUpdateInProgress = true;
-
-      const contentWidth = content.widthSizable ? content.minimumWidth : content.width;
-      const contentHeight = content.heightSizable ? content.minimumHeight : content.height;
-
-      // Our minimum dimensions are directly determined by the content, margins and lineWidth
-      // NOTE: options.minWidth does NOT include the stroke (e.g. lineWidth), left for backward compatibility.
-      this.minimumWidth = Math.max( options.minWidth, contentWidth + ( 2 * options.xMargin ) ) + options.lineWidth;
-      this.minimumHeight = contentHeight + ( 2 * options.yMargin ) + options.lineWidth;
-
-      // Our resulting sizes (allow setting preferred width/height on the panel)
-      const preferredWidth = this.preferredWidth === null ? this.minimumWidth : this.preferredWidth;
-      const preferredHeight = this.preferredHeight === null ? this.minimumHeight : this.preferredHeight;
-
-      // Determine the size available to our content
-      if ( content.widthSizable ) {
-        content.preferredWidth = preferredWidth - options.lineWidth - 2 * options.xMargin;
-      }
-      if ( content.heightSizable ) {
-        content.preferredHeight = preferredHeight - options.lineWidth - 2 * options.yMargin;
-      }
-
-      this.background.setRect( 0, 0, preferredWidth - options.lineWidth, preferredHeight - options.lineWidth );
-
-      // Align the content within the background. If the content width >= minWidth, then all alignments are equivalent.
-      if ( options.align === 'center' ) {
-        content.center = this.background.center;
-      }
-      else if ( options.align === 'left' ) {
-
-        // Use this.background.rectWidth instead of background.width because they differ by the background lineWidth
-        content.left = this.background.centerX - this.background.rectWidth / 2 + options.xMargin;
-        content.centerY = this.background.centerY;
-      }
-      else { /* right */
-
-        // Use this.background.rectWidth instead of background.width because they differ by the background lineWidth
-        content.right = this.background.centerX + this.background.rectWidth / 2 - options.xMargin;
-        content.centerY = this.background.centerY;
-      }
-
-      // clear the recursion-prevention flag
-      backgroundUpdateInProgress = false;
-    };
-
-    if ( options.resize ) {
-      content.boundsProperty.lazyLink( updateBackground );
-      content.visibleProperty.lazyLink( updateBackground );
-      if ( content.widthSizable ) {
-        content.minimumWidthProperty.lazyLink( updateBackground );
-      }
-      if ( content.heightSizable ) {
-        content.minimumHeightProperty.lazyLink( updateBackground );
-      }
-    }
-    this.preferredWidthProperty.lazyLink( updateBackground );
-    this.preferredHeightProperty.lazyLink( updateBackground );
-    updateBackground();
-
-    // @private
-    this.disposePanel = () => {
-      if ( options.resize ) {
-        content.boundsProperty.unlink( updateBackground );
-        content.visibleProperty.unlink( updateBackground );
-        if ( content.widthSizable ) {
-          content.minimumWidthProperty.unlink( updateBackground );
-        }
-        if ( content.heightSizable ) {
-          content.minimumHeightProperty.unlink( updateBackground );
-        }
-      }
-    };
+    // Don't update automatically if resize:false
+    this._constraint.enabled = options.resize;
 
     // Apply options after the layout is done, so that options that use the bounds will work properly.
     this.mutate( options );
   }
 
+  /**
+   * Get the background rectangle's stroke (can be overridden)
+   * @public
+   *
+   * @returns {PaintDef}
+   */
+  getStroke() {
+    return this.background.stroke;
+  }
+
   get stroke() { return this.getStroke(); }
 
+  /**
+   * Change the background rectangle's stroke (can be overridden)
+   * @public
+   *
+   * @param {PaintDef} stroke
+   */
+  setStroke( stroke ) {
+    this.background.stroke = stroke;
+  }
+
   set stroke( value ) { this.setStroke( value ); }
+
+  /**
+   * Get the background rectangle's fill (can be overridden)
+   * @public
+   *
+   * @returns {PaintDef}
+   */
+  getFill() {
+    return this.background.fill;
+  }
+
+  get fill() { return this.getFill(); }
+
+  /**
+   * Change the background rectangle's fill (can be overridden)
+   * @public
+   *
+   * @param {PaintDef} fill
+   */
+  setFill( fill ) {
+    this.background.fill = fill;
+  }
 
   set fill( value ) { this.setFill( value ); }
 
   /**
    * @public
    * @override
+   *
+   * @param {boolean} excludeInvisibleChildrenFromBounds
+   */
+  setExcludeInvisibleChildrenFromBounds( excludeInvisibleChildrenFromBounds ) {
+    super.setExcludeInvisibleChildrenFromBounds( excludeInvisibleChildrenFromBounds );
+
+    this._constraint.updateLayoutAutomatically();
+  }
+
+  /**
+   * Releases references
+   * @public
+   * @override
    */
   dispose() {
-    this.disposePanel();
+    this._constraint.dispose();
+
     super.dispose();
   }
-
-  // @public - Change the background rectangle's stroke (can be overridden)
-  setStroke( stroke ) {
-    this.background.stroke = stroke;
-  }
-
-  // @public - Get the background rectangle's stroke (can be overridden)
-  getStroke() {
-    return this.background.stroke;
-  }
-
-  // @public - Change the background rectangle's fill (can be overridden)
-  setFill( fill ) {
-    this.background.fill = fill;
-  }
-
-  // @public - Get the background rectangle's fill (can be overridden)
-  getFill() {
-    return this.background.fill;
-  }
-
-  get fill() { return this.getFill(); }
 }
 
-// @public (read-only)
+class PanelConstraint extends LayoutConstraint {
+  /**
+   * @param {Panel} panel
+   * @param {Object} [options]
+   */
+  constructor( panel, options ) {
+    super( panel );
+
+    // @private
+    this.panel = panel;
+
+    assert && assert( typeof options.minWidth === 'number', 'Panel minWidth should be a number' );
+    assert && assert( typeof options.xMargin === 'number', 'Panel xMargin should be a number' );
+    assert && assert( typeof options.yMargin === 'number', 'Panel yMargin should be a number' );
+    assert && assert( typeof options.lineWidth === 'number', 'Panel lineWidth should be a number' );
+    assert && assert( ALIGN_VALUES.includes( options.align ), `Panel align should be one of ${ALIGN_VALUES}` );
+
+    // @private {number}
+    this.minWidth = options.minWidth;
+    this.xMargin = options.xMargin;
+    this.yMargin = options.yMargin;
+    this.lineWidth = options.lineWidth;
+    this.align = options.align;
+
+    this.panel.preferredWidthProperty.lazyLink( this._updateLayoutListener );
+    this.panel.preferredHeightProperty.lazyLink( this._updateLayoutListener );
+
+    this.addNode( panel._content );
+  }
+
+  /**
+   * @protected
+   * @override
+   */
+  layout() {
+    super.layout();
+
+    const panel = this.panel;
+    const content = panel._content;
+    const background = panel.background;
+
+    const hasValidContent = panel.isChildIncludedInLayout( content );
+    panel._backgroundContainer.children = hasValidContent ? [ background ] : [];
+
+    if ( !hasValidContent ) {
+      // Bail out (and make the background invisible) if our bounds are invalid
+      panel.minimumWidth = null;
+      panel.minimumHeight = null;
+      return;
+    }
+
+    const contentWidth = ( content.widthSizable && content.minimumWidth !== null ) ? content.minimumWidth : content.width;
+    const contentHeight = ( content.heightSizable && content.minimumHeight !== null ) ? content.minimumHeight : content.height;
+
+    // Our minimum dimensions are directly determined by the content, margins and lineWidth
+    // NOTE: options.minWidth does NOT include the stroke (e.g. lineWidth), left for backward compatibility.
+    const minimumWidth = Math.max( this.minWidth, contentWidth + ( 2 * this.xMargin ) ) + this.lineWidth;
+    const minimumHeight = contentHeight + ( 2 * this.yMargin ) + this.lineWidth;
+
+    // Our resulting sizes (allow setting preferred width/height on the panel)
+    const preferredWidth = panel.preferredWidth === null ? minimumWidth : panel.preferredWidth;
+    const preferredHeight = panel.preferredHeight === null ? minimumHeight : panel.preferredHeight;
+
+    // Determine the size available to our content
+    if ( content.widthSizable ) {
+      content.preferredWidth = preferredWidth - this.lineWidth - 2 * this.xMargin;
+    }
+    if ( content.heightSizable ) {
+      content.preferredHeight = preferredHeight - this.lineWidth - 2 * this.yMargin;
+    }
+
+    background.setRect( 0, 0, preferredWidth - this.lineWidth, preferredHeight - this.lineWidth );
+
+    // Align the content within the background. If the content width >= minWidth, then all alignments are equivalent.
+    if ( this.align === 'center' ) {
+      content.center = background.center;
+    }
+    else if ( this.align === 'left' ) {
+
+      // Use background.rectWidth instead of background.width because they differ by the background lineWidth
+      content.left = background.centerX - background.rectWidth / 2 + this.xMargin;
+      content.centerY = background.centerY;
+    }
+    else { /* right */
+
+      // Use background.rectWidth instead of background.width because they differ by the background lineWidth
+      content.right = background.centerX + background.rectWidth / 2 - this.xMargin;
+      content.centerY = background.centerY;
+    }
+
+    // Set minimums at the end
+    panel.minimumWidth = minimumWidth;
+    panel.minimumHeight = minimumHeight;
+  }
+
+  /**
+   * Releases references
+   * @public
+   * @override
+   */
+  dispose() {
+    this.panel.preferredWidthProperty.unlink( this._updateLayoutListener );
+    this.panel.preferredHeightProperty.unlink( this._updateLayoutListener );
+
+    super.dispose();
+  }
+}
+
+// @public {Object} (read-only)
 Panel.DEFAULT_OPTIONS = DEFAULT_OPTIONS;
 
 sun.register( 'Panel', Panel );
