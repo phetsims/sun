2a295ecfd2c5cd57fec562f13ab3ca266a9894d3
phet-dev <phetadmin@gmail.com>
2022-01-11 03:26:40 -0700

    update copyright dates from daily grunt work

--- js/Slider.js
+++ js/Slider.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2021, University of Colorado Boulder
+// Copyright 2013-2022, University of Colorado Boulder
 
 /**
  * Slider, with support for horizontal and vertical orientations. By default, the slider is constructed in the
