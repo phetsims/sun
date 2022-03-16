59cc29b51469690eef90c89eff49db47b21600e3
pixelzoom <cmalley@pixelzoom.com>
2022-03-16 15:05:34 -0600

    convert PageControl to .ts

--- js/PageControl.ts
+++ js/PageControl.ts
@@ -1,6 +1,5 @@
 // Copyright 2015-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * An iOS-style page control. See the 'Navigation' section of the iOS Human Interface Guidelines.
  * A page control indicates the number of pages and which one is currently visible.
@@ -8,45 +7,64 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import IProperty from '../../axon/js/IProperty.js';
 import { Shape } from '../../kite/js/imports.js';
-import merge from '../../phet-core/js/merge.js';
-import { PressListener } from '../../scenery/js/imports.js';
-import { Circle } from '../../scenery/js/imports.js';
-import { Node } from '../../scenery/js/imports.js';
+import optionize from '../../phet-core/js/optionize.js';
+import { Circle, CircleOptions, IColor, Node, NodeOptions, PressListener, PressListenerEvent } from '../../scenery/js/imports.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import sun from './sun.js';
 
+type SelfOptions = {
+  interactive?: boolean; // {boolean} whether the control is interactive
+
+  // all dots
+  orientation?: 'horizontal' | 'vertical';
+  dotRadius?: number; // {number} radius of the dots
+  lineWidth?: number;
+  dotSpacing?: number; // {number} spacing between dots
+  dotTouchAreaDilation?: number; // {number} how much to dilate the touchArea beyond the radius of a dot
+  dotMouseAreaDilation?: number; // {number} how much to dilate the mouseArea beyond the radius of a dot
+
+  // dots representing the current page
+  currentPageFill?: IColor;
+  currentPageStroke?: IColor;
+
+  // dots representing all pages except the current page
+  pageFill?: IColor;
+  pageStroke?: IColor;
+};
+
+export type PageControlOptions = SelfOptions & Omit<NodeOptions, 'children'>;
+
 class PageControl extends Node {
 
+  private readonly disposePageControl: () => void;
+
   /**
-   * @param {number} numberOfPages - number of pages
-   * @param {Property.<number>} pageNumberProperty - which page is currently visible
-   * @param {Object} [options]
+   * @param numberOfPages - number of pages
+   * @param pageNumberProperty - which page is currently visible
+   * @param providedOptions
    */
-  constructor( numberOfPages, pageNumberProperty, options ) {
+  constructor( numberOfPages: number, pageNumberProperty: IProperty<number>, providedOptions: PageControlOptions ) {
 
-    options = merge( {
+    const options = optionize<PageControlOptions, SelfOptions, NodeOptions, 'tandem'>( {
 
-      interactive: false, // {boolean} whether the control is interactive
-
-      // all dots
+      // SelfOptions
+      interactive: false,
       orientation: 'horizontal',
-      dotRadius: 3, // {number} radius of the dots
+      dotRadius: 3,
       lineWidth: 1,
-      dotSpacing: 10, // {number} spacing between dots
-      dotTouchAreaDilation: 0, // {number} how much to dilate the touchArea beyond the radius of a dot
-      dotMouseAreaDilation: 0, // {number} how much to dilate the mouseArea beyond the radius of a dot
-
-      // dots representing the current page
-      currentPageFill: 'black', // {Color|string} dot color for the page that is visible
+      dotSpacing: 10,
+      dotTouchAreaDilation: 0,
+      dotMouseAreaDilation: 0,
+      currentPageFill: 'black',
       currentPageStroke: null,
-
-      // dots representing all pages except the current page
-      pageFill: 'rgb( 200, 200, 200 )', // {Color|string} dot color for pages that are not visible
+      pageFill: 'rgb( 200, 200, 200 )',
       pageStroke: null,
 
+      // NodeOptions
       tandem: Tandem.REQUIRED
-    }, options );
+    }, providedOptions );
 
     // validate options
     assert && assert( _.includes( [ 'horizontal', 'vertical' ], options.orientation ), `invalid orientation=${options.orientation}` );
@@ -56,18 +74,18 @@ class PageControl extends Node {
 
     // Clicking on a dot goes to that page
     const pressListener = new PressListener( {
-      tandem: options.tandem.createTandem( 'pressListener' ),
-      press: event => {
-        assert && assert( event.currentTarget.hasOwnProperty( 'pageNumber' ) );
-        pageNumberProperty.set( event.currentTarget.pageNumber );
-      }
+      press: ( event: PressListenerEvent ) => {
+        if ( event.currentTarget instanceof DotNode ) {
+          pageNumberProperty.value = event.currentTarget.pageNumber;
+        }
+      },
+      tandem: options.tandem.createTandem( 'pressListener' )
     } );
 
     // Create a dot for each page.
-    // Add them to an intermediate parent node, so that additional children can't be inadvertently added.
     // For horizontal orientation, pages are ordered left-to-right.
     // For vertical orientation, pages are ordered top-to-bottom.
-    const dotsParent = new Node();
+    const dotNodes: DotNode[] = [];
     for ( let pageNumber = 0; pageNumber < numberOfPages; pageNumber++ ) {
 
       // dot
@@ -79,7 +97,7 @@ class PageControl extends Node {
         x: isHorizontal ? dotCenter : 0,
         y: isHorizontal ? 0 : dotCenter
       } );
-      dotsParent.addChild( dotNode );
+      dotNodes.push( dotNode );
 
       // pointer areas
       dotNode.touchArea = ( options.dotTouchAreaDilation === 0 ) ? null : Shape.circle( 0, 0, options.dotRadius + options.dotTouchAreaDilation );
@@ -93,22 +111,22 @@ class PageControl extends Node {
     }
 
     // Indicate which page is selected
-    const pageNumberObserver = ( pageNumber, oldPageNumber ) => {
+    const pageNumberObserver = ( pageNumber: number, oldPageNumber: number | null ) => {
 
       // previous dot
-      if ( oldPageNumber || oldPageNumber === 0 ) {
-        dotsParent.getChildAt( oldPageNumber ).fill = options.pageFill;
-        dotsParent.getChildAt( oldPageNumber ).stroke = options.pageStroke;
+      if ( oldPageNumber !== null ) {
+        dotNodes[ oldPageNumber ].fill = options.pageFill;
+        dotNodes[ oldPageNumber ].stroke = options.pageStroke;
       }
 
       // current dot
-      dotsParent.getChildAt( pageNumber ).fill = options.currentPageFill;
-      dotsParent.getChildAt( pageNumber ).stroke = options.currentPageStroke;
+      dotNodes[ pageNumber ].fill = options.currentPageFill;
+      dotNodes[ pageNumber ].stroke = options.currentPageStroke;
     };
     pageNumberProperty.link( pageNumberObserver );
 
     assert && assert( !options.children, 'PageControl sets children' );
-    options.children = [ dotsParent ];
+    options.children = dotNodes;
 
     super( options );
 
@@ -133,12 +151,15 @@ class PageControl extends Node {
  */
 class DotNode extends Circle {
 
+  // the page number associated with this dot
+  public readonly pageNumber: number;
+
   /**
    * @param {number} pageNumber - page number that the dot is associated with
    * @param {number} radius
    * @param {Object} [options]
    */
-  constructor( pageNumber, radius, options ) {
+  constructor( pageNumber: number, radius: number, options: CircleOptions ) {
     super( radius, options );
     this.pageNumber = pageNumber; // @public (read-only)
   }
