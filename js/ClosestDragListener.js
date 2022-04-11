ce00808cf08c03614b4fd64846326a5ea54496a0
pixelzoom <cmalley@pixelzoom.com>
2022-04-11 11:41:23 -0600

    // @ts-nocheck

--- js/ClosestDragListener.js
+++ js/ClosestDragListener.js
@@ -1,5 +1,6 @@
 // Copyright 2016-2021, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * A Scenery input listener that is able to find the closest in a list of items to a "down" event and trigger an action
  * (usually a drag) on that item. Usually this will be a drag listener start/press (e.g. SimpleDragHandler/DragListener),
