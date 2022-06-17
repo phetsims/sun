75c38aa2e4c15fe8d7a937456e974e10cadddbc9
Marla Schulz <marlaa.schulz@gmail.com>
2022-06-17 16:23:08 -0600

    Enable no-empty-functions and prevent empty functions, see: https://github.com/phetsims/chipper/issues/1249

--- js/ToggleNode.ts
+++ js/ToggleNode.ts
@@ -161,7 +161,9 @@ export default class ToggleNode<T> extends Node {
    * A value for the alignChildren option.
    * No alignment is performed.
    */
-  public static NONE( children: Node[] ): void {}
+  public static NONE( children: Node[] ): void {
+    // Nothing to do
+  }
 }
 
 sun.register( 'ToggleNode', ToggleNode );
