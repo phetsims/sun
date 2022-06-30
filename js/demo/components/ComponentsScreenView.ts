8286656588898155fd6b02f9ab849ffa9ab49a00
pixelzoom <cmalley@pixelzoom.com>
2022-06-29 19:26:21 -0600

    factor out demoSlider.ts

--- js/demo/components/ComponentsScreenView.ts
+++ js/demo/components/ComponentsScreenView.ts
@@ -14,7 +14,6 @@ import Property from '../../../../axon/js/Property.js';
 import stepTimer from '../../../../axon/js/stepTimer.js';
 import StringProperty from '../../../../axon/js/StringProperty.js';
 import Bounds2 from '../../../../dot/js/Bounds2.js';
-import Dimension2 from '../../../../dot/js/Dimension2.js';
 import dotRandom from '../../../../dot/js/dotRandom.js';
 import Range from '../../../../dot/js/Range.js';
 import optionize, { combineOptions } from '../../../../phet-core/js/optionize.js';
@@ -22,28 +21,24 @@ import EmptyObjectType from '../../../../phet-core/js/types/EmptyObjectType.js';
 import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import { AlignBox, AlignGroup, HBox, Node, Rectangle, Text, VBox } from '../../../../scenery/js/imports.js';
-import { PhetioObjectOptions } from '../../../../tandem/js/PhetioObject.js';
-import Tandem from '../../../../tandem/js/Tandem.js';
 import AccordionBox from '../../AccordionBox.js';
 import RectangularPushButton from '../../buttons/RectangularPushButton.js';
 import Carousel from '../../Carousel.js';
 import Checkbox from '../../Checkbox.js';
 import ComboBox, { ComboBoxItem } from '../../ComboBox.js';
-import HSlider, { HSliderOptions } from '../../HSlider.js';
 import NumberSpinner, { NumberSpinnerOptions } from '../../NumberSpinner.js';
 import OnOffSwitch from '../../OnOffSwitch.js';
 import PageControl from '../../PageControl.js';
 import Panel from '../../Panel.js';
-import Slider, { SliderOptions } from '../../Slider.js';
 import sun from '../../sun.js';
 import sunQueryParameters from '../../sunQueryParameters.js';
 import ToggleSwitch from '../../ToggleSwitch.js';
-import VSlider, { VSliderOptions } from '../../VSlider.js';
 import DemosScreenView, { DemosScreenViewOptions } from '../DemosScreenView.js';
 import NumberPicker from '../../NumberPicker.js';
 import demoABSwitch from './demoABSwitch.js';
 import demoCarousel from './demoCarousel.js';
 import demoCheckbox from './demoCheckbox.js';
+import { demoHSlider, demoVSlider } from './demoSlider.js';
 
 type SelfOptions = EmptyObjectType;
 type ButtonsScreenViewOptions = SelfOptions & PickRequired<DemosScreenViewOptions, 'tandem'>;
@@ -81,7 +76,6 @@ export default class ComponentsScreenView extends DemosScreenView {
   }
 }
 
-// Creates a demo of ComboBox
 function demoComboBox( layoutBounds: Bounds2 ): Node {
 
   const values = [ 'one', 'two', 'three', 'four', 'five', 'six' ];
@@ -116,170 +110,18 @@ function demoComboBox( layoutBounds: Bounds2 ): Node {
   return new Node( { children: [ uiComponents, listParent ] } );
 }
 
-// Creates a demo for HSlider
-function demoHSlider( layoutBounds: Bounds2, options?: PhetioObjectOptions ): Node {
-  return demoSlider( layoutBounds, 'horizontal', options );
-}
-
-// Creates a demo for VSlider
-function demoVSlider( layoutBounds: Bounds2, options?: PhetioObjectOptions ): Node {
-  return demoSlider( layoutBounds, 'vertical', options );
-}
-
-/**
- * Used by demoHSlider and demoVSlider
- */
-function demoSlider( layoutBounds: Bounds2, orientation: 'horizontal' | 'vertical', providedOptions?: SliderOptions ): Node {
-
-  const options = combineOptions<SliderOptions>( {
-    center: layoutBounds.center,
-    tandem: Tandem.REQUIRED,
-    phetioDesigned: true
-  }, providedOptions );
-
-  const property = new Property( 0 );
-  const range = new Range( 0, 100 );
-  const tickLabelOptions = { font: new PhetFont( 16 ) };
-
-  const enabledRangeProperty = new Property( new Range( 0, 100 ) );
-
-  let slider: Slider;
-  if ( orientation === 'horizontal' ) {
-    slider = new HSlider( property, range, combineOptions<HSliderOptions>( options, {
-      trackSize: new Dimension2( 300, 5 ),
-
-      // Demonstrate larger x dilation.
-      thumbTouchAreaXDilation: 30,
-      thumbTouchAreaYDilation: 15,
-      thumbMouseAreaXDilation: 10,
-      thumbMouseAreaYDilation: 5,
-      enabledRangeProperty: enabledRangeProperty,
-
-      phetioEnabledPropertyInstrumented: false
-    } ) );
-  }
-  else {
-    slider = new VSlider( property, range, combineOptions<VSliderOptions>( options, {
-      trackSize: new Dimension2( 5, 300 ),
-
-      // Demonstrate larger y dilation, to verify that VSlider is handling things correctly.
-      thumbTouchAreaXDilation: 15,
-      thumbTouchAreaYDilation: 30,
-      thumbMouseAreaXDilation: 5,
-      thumbMouseAreaYDilation: 10,
-      enabledRangeProperty: enabledRangeProperty,
-
-      phetioEnabledPropertyInstrumented: false
-    } ) );
-  }
-
-  // Settable
-  const enabledProperty = new BooleanProperty( true );
-  slider.enabledProperty = enabledProperty;
-
-  // major ticks
-  slider.addMajorTick( range.min, new Text( range.min, tickLabelOptions ) );
-  slider.addMajorTick( range.getCenter(), new Text( range.getCenter(), tickLabelOptions ) );
-  slider.addMajorTick( range.max, new Text( range.max, tickLabelOptions ) );
-
-  // minor ticks
-  slider.addMinorTick( range.min + 0.25 * range.getLength() );
-  slider.addMinorTick( range.min + 0.75 * range.getLength() );
-
-  // show/hide major ticks
-  const majorTicksVisibleProperty = new Property( true );
-  majorTicksVisibleProperty.link( visible => {
-    slider.majorTicksVisible = visible;
-  } );
-  const majorTicksCheckbox = new Checkbox( majorTicksVisibleProperty, new Text( 'Major ticks visible', { font: new PhetFont( 20 ) } ), {
-    tandem: Tandem.OPT_OUT,
-    left: slider.left,
-    top: slider.bottom + 40
-  } );
-
-  // show/hide minor ticks
-  const minorTicksVisibleProperty = new Property( true );
-  minorTicksVisibleProperty.link( visible => {
-    slider.minorTicksVisible = visible;
-  } );
-  const minorTicksCheckbox = new Checkbox( minorTicksVisibleProperty, new Text( 'Minor ticks visible', { font: new PhetFont( 20 ) } ), {
-    tandem: Tandem.OPT_OUT,
-    left: slider.left,
-    top: majorTicksCheckbox.bottom + 40
-  } );
-
-  // Checkbox to enable/disable slider
-  const enabledCheckbox = new Checkbox( enabledProperty, new Text( 'Enable slider', { font: new PhetFont( 20 ) } ), {
-    tandem: Tandem.OPT_OUT,
-    left: slider.left,
-    top: minorTicksCheckbox.bottom + 40
-  } );
-
-  // restrict enabled range of slider
-  const restrictedRangeProperty = new Property( false );
-  restrictedRangeProperty.link( restrictedRange => {
-    enabledRangeProperty.value = restrictedRange ? new Range( 25, 75 ) : new Range( 0, 100 );
-  } );
-
-  const enabledRangeCheckbox = new Checkbox( restrictedRangeProperty, new Text( 'Enable Range [25, 75]', { font: new PhetFont( 20 ) } ), {
-    tandem: Tandem.OPT_OUT,
-    left: slider.left,
-    top: enabledCheckbox.bottom + 40
-  } );
-
-  // If the user is holding down the thumb outside of the enabled range, and the enabled range expands, the value should
-  // adjust to the new extremum of the range, see https://github.com/phetsims/mean-share-and-balance/issues/29
-  const animateEnabledRangeProperty = new BooleanProperty( false );
-  const animateEnabledRangeCheckbox = new Checkbox( animateEnabledRangeProperty, new Text( 'Animate Enabled Range', { font: new PhetFont( 20 ) } ), {
-    tandem: Tandem.OPT_OUT,
-    left: slider.left,
-    top: enabledRangeCheckbox.bottom + 40
-  } );
-
-  stepTimer.addListener( () => {
-    if ( animateEnabledRangeProperty.value ) {
-      enabledRangeProperty.value = new Range( Math.max( enabledRangeProperty.value.min - 0.1, 0 ), 75 );
-    }
-  } );
-
-  // All of the controls related to the slider
-  const controls = new VBox( {
-    align: 'left',
-    spacing: 30,
-    children: [ majorTicksCheckbox, minorTicksCheckbox, enabledCheckbox, enabledRangeCheckbox, animateEnabledRangeCheckbox ]
-  } );
-
-  // Position the control based on the orientation of the slider
-  const boxOptions = {
-    spacing: 60,
-    children: [ slider, controls ],
-    center: layoutBounds.center
-  };
-  let box = null;
-  if ( orientation === 'horizontal' ) {
-    box = new VBox( boxOptions );
-  }
-  else {
-    box = new HBox( boxOptions );
-  }
-
-  return box;
-}
-
 function demoToggleSwitch( layoutBounds: Bounds2 ): Node {
   return new ToggleSwitch( new StringProperty( 'left' ), 'left', 'right', {
     center: layoutBounds.center
   } );
 }
 
-// Creates a demo for OnOffSwitch
 function demoOnOffSwitch( layoutBounds: Bounds2 ): Node {
   return new OnOffSwitch( new BooleanProperty( true ), {
     center: layoutBounds.center
   } );
 }
 
-// Creates a demo for PageControl
 function demoPageControl( layoutBounds: Bounds2 ): Node {
 
   // create items
@@ -315,7 +157,6 @@ function demoPageControl( layoutBounds: Bounds2 ): Node {
   } );
 }
 
-// Creates a demo for NumberSpinner
 function demoNumberSpinner( layoutBounds: Bounds2 ): Node {
 
   const valueProperty = new Property( 0 );
@@ -389,7 +230,6 @@ function demoNumberSpinner( layoutBounds: Bounds2 ): Node {
   } );
 }
 
-// Creates a demo for NumberPicker
 function demoNumberPicker( layoutBounds: Bounds2 ): Node {
 
   const enabledProperty = new BooleanProperty( true );
---
+++ js/demo/components/demoSlider.ts
@@ -0,0 +1,176 @@
+// Copyright 2022, University of Colorado Boulder
+
+/**
+ * Demos for HSlider and VSlider
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+
+import Slider, { SliderOptions } from '../../Slider.js';
+import HSlider, { HSliderOptions } from '../../HSlider.js';
+import VSlider, { VSliderOptions } from '../../VSlider.js';
+import Checkbox from '../../Checkbox.js';
+import Bounds2 from '../../../../dot/js/Bounds2.js';
+import Range from '../../../../dot/js/Range.js';
+import { PhetioObjectOptions } from '../../../../tandem/js/PhetioObject.js';
+import { combineOptions } from '../../../../phet-core/js/optionize.js';
+import Tandem from '../../../../tandem/js/Tandem.js';
+import Property from '../../../../axon/js/Property.js';
+import Dimension2 from '../../../../dot/js/Dimension2.js';
+import { Font, HBox, Node, Text, VBox } from '../../../../scenery/js/imports.js';
+import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
+import stepTimer from '../../../../axon/js/stepTimer.js';
+
+const CHECKBOX_FONT = new Font( { size: 20 } );
+
+// Used by demoHSlider and demoVSlider
+function demoSlider( layoutBounds: Bounds2, orientation: 'horizontal' | 'vertical', providedOptions?: SliderOptions ): Node {
+
+  const options = combineOptions<SliderOptions>( {
+    center: layoutBounds.center,
+    tandem: Tandem.REQUIRED,
+    phetioDesigned: true
+  }, providedOptions );
+
+  const property = new Property( 0 );
+  const range = new Range( 0, 100 );
+  const tickLabelOptions = { font: new Font( { size: 16 } ) };
+
+  const enabledRangeProperty = new Property( new Range( 0, 100 ) );
+
+  let slider: Slider;
+  if ( orientation === 'horizontal' ) {
+    slider = new HSlider( property, range, combineOptions<HSliderOptions>( options, {
+      trackSize: new Dimension2( 300, 5 ),
+
+      // Demonstrate larger x dilation.
+      thumbTouchAreaXDilation: 30,
+      thumbTouchAreaYDilation: 15,
+      thumbMouseAreaXDilation: 10,
+      thumbMouseAreaYDilation: 5,
+      enabledRangeProperty: enabledRangeProperty,
+
+      phetioEnabledPropertyInstrumented: false
+    } ) );
+  }
+  else {
+    slider = new VSlider( property, range, combineOptions<VSliderOptions>( options, {
+      trackSize: new Dimension2( 5, 300 ),
+
+      // Demonstrate larger y dilation, to verify that VSlider is handling things correctly.
+      thumbTouchAreaXDilation: 15,
+      thumbTouchAreaYDilation: 30,
+      thumbMouseAreaXDilation: 5,
+      thumbMouseAreaYDilation: 10,
+      enabledRangeProperty: enabledRangeProperty,
+
+      phetioEnabledPropertyInstrumented: false
+    } ) );
+  }
+
+  // Settable
+  const enabledProperty = new BooleanProperty( true );
+  slider.enabledProperty = enabledProperty;
+
+  // major ticks
+  slider.addMajorTick( range.min, new Text( range.min, tickLabelOptions ) );
+  slider.addMajorTick( range.getCenter(), new Text( range.getCenter(), tickLabelOptions ) );
+  slider.addMajorTick( range.max, new Text( range.max, tickLabelOptions ) );
+
+  // minor ticks
+  slider.addMinorTick( range.min + 0.25 * range.getLength() );
+  slider.addMinorTick( range.min + 0.75 * range.getLength() );
+
+  // show/hide major ticks
+  const majorTicksVisibleProperty = new Property( true );
+  majorTicksVisibleProperty.link( visible => {
+    slider.majorTicksVisible = visible;
+  } );
+  const majorTicksCheckbox = new Checkbox( majorTicksVisibleProperty,
+    new Text( 'Major ticks visible', { font: CHECKBOX_FONT } ), {
+      tandem: Tandem.OPT_OUT,
+      left: slider.left,
+      top: slider.bottom + 40
+    } );
+
+  // show/hide minor ticks
+  const minorTicksVisibleProperty = new Property( true );
+  minorTicksVisibleProperty.link( visible => {
+    slider.minorTicksVisible = visible;
+  } );
+  const minorTicksCheckbox = new Checkbox( minorTicksVisibleProperty,
+    new Text( 'Minor ticks visible', { font: CHECKBOX_FONT } ), {
+      tandem: Tandem.OPT_OUT,
+      left: slider.left,
+      top: majorTicksCheckbox.bottom + 40
+    } );
+
+  // Checkbox to enable/disable slider
+  const enabledCheckbox = new Checkbox( enabledProperty,
+    new Text( 'Enable slider', { font: CHECKBOX_FONT } ), {
+      tandem: Tandem.OPT_OUT,
+      left: slider.left,
+      top: minorTicksCheckbox.bottom + 40
+    } );
+
+  // restrict enabled range of slider
+  const restrictedRangeProperty = new Property( false );
+  restrictedRangeProperty.link( restrictedRange => {
+    enabledRangeProperty.value = restrictedRange ? new Range( 25, 75 ) : new Range( 0, 100 );
+  } );
+
+  const enabledRangeCheckbox = new Checkbox( restrictedRangeProperty,
+    new Text( 'Enable Range [25, 75]', { font: CHECKBOX_FONT } ), {
+      tandem: Tandem.OPT_OUT,
+      left: slider.left,
+      top: enabledCheckbox.bottom + 40
+    } );
+
+  // If the user is holding down the thumb outside of the enabled range, and the enabled range expands, the value should
+  // adjust to the new extremum of the range, see https://github.com/phetsims/mean-share-and-balance/issues/29
+  const animateEnabledRangeProperty = new BooleanProperty( false );
+  const animateEnabledRangeCheckbox = new Checkbox( animateEnabledRangeProperty, new Text( 'Animate Enabled Range', { font: CHECKBOX_FONT } ), {
+    tandem: Tandem.OPT_OUT,
+    left: slider.left,
+    top: enabledRangeCheckbox.bottom + 40
+  } );
+
+  stepTimer.addListener( () => {
+    if ( animateEnabledRangeProperty.value ) {
+      enabledRangeProperty.value = new Range( Math.max( enabledRangeProperty.value.min - 0.1, 0 ), 75 );
+    }
+  } );
+
+  // All of the controls related to the slider
+  const controls = new VBox( {
+    align: 'left',
+    spacing: 30,
+    children: [ majorTicksCheckbox, minorTicksCheckbox, enabledCheckbox, enabledRangeCheckbox, animateEnabledRangeCheckbox ]
+  } );
+
+  // Position the control based on the orientation of the slider
+  const boxOptions = {
+    spacing: 60,
+    children: [ slider, controls ],
+    center: layoutBounds.center
+  };
+  let box = null;
+  if ( orientation === 'horizontal' ) {
+    box = new VBox( boxOptions );
+  }
+  else {
+    box = new HBox( boxOptions );
+  }
+
+  return box;
+}
+
+// Creates a demo for HSlider
+export function demoHSlider( layoutBounds: Bounds2, options?: PhetioObjectOptions ): Node {
+  return demoSlider( layoutBounds, 'horizontal', options );
+}
+
+// Creates a demo for VSlider
+export function demoVSlider( layoutBounds: Bounds2, options?: PhetioObjectOptions ): Node {
+  return demoSlider( layoutBounds, 'vertical', options );
+}
