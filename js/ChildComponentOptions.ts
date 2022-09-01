d5b351255cdd1cc510dcb672d052abc0782a0976
phet-dev <phetadmin@gmail.com>
2022-09-01 04:45:47 -0600

    update copyright dates from daily grunt work

--- js/ChildComponentOptions.ts
+++ js/ChildComponentOptions.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2022, University of Colorado Boulder
+// Copyright 2022, University of Colorado Boulder
 
 /**
  * Defines the type for when you can pass in a Node or a function that creates a Node from a Tandem.
