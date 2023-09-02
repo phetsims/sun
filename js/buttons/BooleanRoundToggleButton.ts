330140b63fbf77193ce114e6f95e469c8b33d539
phet-dev <phetadmin@gmail.com>
2023-09-02 03:20:32 -0600

    update copyright dates from daily grunt work

--- js/buttons/BooleanRoundToggleButton.ts
+++ js/buttons/BooleanRoundToggleButton.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2022, University of Colorado Boulder
+// Copyright 2013-2023, University of Colorado Boulder
 
 /**
  * This toggle button uses a boolean Property and a trueNode and falseNode to display its content.
