3ad2b04508b23551e2d368222e1269fdc7d9274f
AgustinVallejo <agustin.vallejo@udea.edu.co>
2024-09-13 15:42:47 -0500

    Adding deselectedFill to RadioButtons to implement a Classical|Quantum selector as designed, see https://github.com/phetsims/quantum-measurement/issues/10

--- js/buttons/RectangularRadioButton.ts
+++ js/buttons/RectangularRadioButton.ts
@@ -211,6 +211,7 @@ export default class RectangularRadioButton<T> extends RectangularButton {
         deselectedButtonOpacity: 1,
         deselectedLineWidth: 1,
         deselectedStroke: 'gray',
+        deselectedFill: ColorConstants.LIGHT_BLUE,
         overButtonOpacity: 0.8,
         overFill: null,
         overLineWidth: 0,
@@ -255,7 +256,7 @@ export default class RectangularRadioButton<T> extends RectangularButton {
             break;
 
           case RadioButtonInteractionState.DESELECTED:
-            buttonBackground.fill = baseColorProperty;
+            buttonBackground.fill = options.deselectedFill;
             buttonBackground.stroke = options.deselectedStroke;
             buttonBackground.lineWidth = options.deselectedLineWidth;
             buttonBackground.opacity = options.deselectedButtonOpacity;
