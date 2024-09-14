e8b8c6dc0b31b408b96156a1aebc4bff9bcad0f3
phet-dev <phetadmin@gmail.com>
2024-09-14 04:30:48 -0600

    update copyright dates from daily grunt work

--- js/buttons/TButtonAppearanceStrategy.ts
+++ js/buttons/TButtonAppearanceStrategy.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * Type to annotate the constructor signature of ButtonAppearanceStrategy
