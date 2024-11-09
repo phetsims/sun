3a56da6c1f8dbdac1306b9f0f247c018c01e2c4f
phet-dev <phetadmin@gmail.com>
2024-11-09 03:35:36 -0700

    update copyright dates from daily grunt work

--- js/buttons/RadioButtonInteractionStateProperty.ts
+++ js/buttons/RadioButtonInteractionStateProperty.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2024, University of Colorado Boulder
 
 /**
  * A DerivedProperty that maps ButtonModel states to the states needed by the radio button view.
