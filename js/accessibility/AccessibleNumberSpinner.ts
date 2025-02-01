89ff0f3a1b2c281b99c980863ede7faa172926cc
phet-dev <phetadmin@gmail.com>
2025-02-01 03:44:43 -0700

    update copyright dates from daily grunt work

--- js/accessibility/AccessibleNumberSpinner.ts
+++ js/accessibility/AccessibleNumberSpinner.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2024, University of Colorado Boulder
+// Copyright 2018-2025, University of Colorado Boulder
 
 /**
  * A trait for subtypes of Node, used to make the Node behave like a 'number' input with assistive technology.
