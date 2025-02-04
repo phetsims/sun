8089503efcbfd3abfcb13d36a9bd684c91466b22
phet-dev <phetadmin@gmail.com>
2025-02-04 04:42:11 -0700

    update copyright dates from daily grunt work

--- js/buttons/RectangularRadioButtonGroup.ts
+++ js/buttons/RectangularRadioButtonGroup.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2024, University of Colorado Boulder
+// Copyright 2014-2025, University of Colorado Boulder
 
 /**
  * RectangularRadioButtonGroup is a group of rectangular radio buttons, in either horizontal or vertical orientation.
