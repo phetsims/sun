6ce130344cd451cd2a0fd66ec1eacee640f575e3
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-25 16:33:22 -0600

    runnable *QueryParameters.ts files to use QueryStringMachineModule, https://github.com/phetsims/query-string-machine/issues/54

--- js/sunQueryParameters.ts
+++ js/sunQueryParameters.ts
@@ -6,6 +6,7 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import { QueryStringMachine } from '../../query-string-machine/js/QueryStringMachineModule.js';
 import sun from './sun.js';
 
 const sunQueryParameters = QueryStringMachine.getAll( {
