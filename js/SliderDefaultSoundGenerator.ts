1624e090553d879e700ca9c21aaad07cd0f32db6
phet-dev <phetadmin@gmail.com>
2026-02-18 04:15:02 -0700

    update copyright dates from daily grunt work

--- js/SliderDefaultSoundGenerator.ts
+++ js/SliderDefaultSoundGenerator.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2026, University of Colorado Boulder
 
 /**
  * The default sound used for Slider if options.soundGenerator is not set.
